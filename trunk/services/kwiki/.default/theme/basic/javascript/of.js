4641f7f008015c618931d62f1141c2ea578ce8fd
wangaguo <wangaguo@eab794a4-67ee-0310-8774-9eb661efe622>
2006-11-16 07:44:01 +0000

    openfoundry modifications

---
+++ trunk/services/kwiki/.default/theme/basic/javascript/of.js
@@ -0,0 +1,17 @@
+<!-- Place in iframe content html file by wangaguo-->
+<!--
+	function iframeAutoFit()
+	{
+		parent.document.getElementById("wiki").style.height = this.document.body.scrollHeight+50;
+	}
+	
+	if(window.attachEvent)
+	{
+		window.attachEvent("onload",  iframeAutoFit);
+	}
+	else if(window.addEventListener)
+	{
+		window.addEventListener('load',  iframeAutoFit,  false);
+	}
+	
+//-->
