490e92f238e6c801a7c8d8ddde6ff210db752d9c
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2007-03-02 08:56:38 +0000

    chmod

--- trunk/services/sympa/openfoundry/subst.sh
+++ trunk/services/sympa/openfoundry/subst.sh
@@ -4,7 +4,7 @@ sympa_co=/usr/local/checkout/trunk/services/sympa
 
 mysubst()
 {
-	env INSTALLDIR= INSTALLNAME=$2 `cat $sympa_co/openfoundry/subst.env` perl $sympa_co/source/subst.pl $1
+	env INSTALLDIR= INSTALLNAME=$2 UMASK=$3 `cat $sympa_co/openfoundry/subst.env` perl $sympa_co/source/subst.pl $1
 }
 
-mysubst $sympa_co/source/wwsympa/wwsympa.fcgi /usr/local/sympa/cgi-bin/wwsympa.fcgi
+mysubst $sympa_co/source/wwsympa/wwsympa.fcgi /usr/local/sympa/cgi-bin/wwsympa.fcgi 0755
