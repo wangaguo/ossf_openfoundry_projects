61497635f5c9ec11ec6c009911797d5887751d8b
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2007-04-25 08:17:50 +0000

    create table project_users

--- branches/0.1/db/migrate/002_create_project_users.rb
+++ branches/0.1/db/migrate/002_create_project_users.rb
@@ -1,6 +1,9 @@
 class CreateProjectUsers < ActiveRecord::Migration
   def self.up
     create_table :project_users do |t|
+      t.column :project_id, :integer
+      t.column :user_id, :integer
+      t.column :role, :string
     end
   end
 
