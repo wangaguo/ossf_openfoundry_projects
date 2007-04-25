a89f3fdca59535f0c6984615869a5f5a9865bcb1
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2007-04-25 03:56:12 +0000

    "Include the SHLG system into your application controller"

--- branches/0.1/app/controllers/application.rb
+++ branches/0.1/app/controllers/application.rb
@@ -1,7 +1,19 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 
+require 'localization'
+require 'user_system'
+
 class ApplicationController < ActionController::Base
+  include Localization
+  include UserSystem
+
+  helper :user
+  model  :user
+
+  before_filter :login_required
+
+  
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_of_session_id'
 end
