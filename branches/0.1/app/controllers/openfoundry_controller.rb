d0f99f3655e7911bca4222220c999b417de208b0
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2007-05-15 02:43:16 +0000

    generate json data for integration

--- branches/0.1/app/controllers/openfoundry_controller.rb
+++ branches/0.1/app/controllers/openfoundry_controller.rb
@@ -1,5 +1,47 @@
+require "rubygems"
+require "json"
+
 class OpenfoundryController < ApplicationController
 
   def index
+  end
+
+  # TODO: optimize!!!!!!!!!!
+  def authentication_authorization
+    #self.class.layout(nil)
+    session_id, project_unixname = params[:SID], params[:projectUnixName]
+    if the_session = Session.find_by_session_id(session_id)
+      the_session_data = Marshal.load(Base64.decode64(the_session.data))
+      user = the_session_data['user']
+      @name = user.login
+      project = Project.find_by_unixname(project_unixname)
+      if project == nil
+        @role = "Other"
+      elsif user.has_role?('Admin', project)
+        @role = "Admin"
+      elsif user.has_role?('Member', project)
+        @role = "Member"
+      else
+        @role = "Other"
+      end
+      @email = user.email
+    else
+      #guest Other guest@users.openfoundry.org
+      @name = "guest"
+      @role = "Other"
+      @email = "guest@users.openfoundry.org"
+    end
+    render :text => "#{@name} #{@role} #{@email}" , :layout => false
+  end
+  def foundry_dump # TODO: optimize !!!!
+    data = {
+      :projects => Project.find_all().map { |p| { :Id => p.id, :ProjectName => p.projectname , :UnixName => p.unixname } },
+      :users => User.find_all().map { |u| { :Id => u.id, :Name => u.login, :Email => u.email } },
+      :relations => {
+        :admin => Project.find_all().inject([]) { |all, p| all + p.admins().map { |u| [p.id, u.id] } },
+        :member => Project.find_all().inject([]) { |all, p| all + p.members().map { |u| [p.id, u.id] } }
+      }
+    }
+    render :text => data.to_json, :layout => false
   end
 end
