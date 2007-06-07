1d3eef9a33d48429c7ff3483e8c5585cf217f379
LCamel <LCamel@users.noreply.github.com>
2007-06-07 08:23:54 +0000

    add pam-mysql for mod_auth_pam2 (NOTE: mod_auth_pam2 + pam_unix + libnss-mysql can't work correctly with uid 80)

--- services/vcs/install.sh
+++ services/vcs/install.sh
@@ -8,3 +8,6 @@ portsnap extract
 ( cd /usr/ports/databases/mysql50-server ; make WITH_CHARSET=utf8 WITH_XCHARSET=complex WITH_COLLATION=utf8_general_ci install )
 ( cd /usr/ports/net/libnss-mysql ; make install )
 ( cd /usr/ports/www/mod_python3 ; make BATCH=yes install )
+( cd /usr/ports/security/pam-mysql ; make install )
+#ln -sf /usr/local/lib/pam_mysql.so /usr/lib/pam_mysql.so
+( cd /usr/ports/www/mod_auth_pam2 ; make install)
