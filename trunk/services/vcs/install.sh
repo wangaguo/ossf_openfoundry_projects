662ac0e23d2c272667751e37340424c7174ab95a
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2007-06-07 07:00:45 +0000

    mod_python

--- trunk/services/vcs/install.sh
+++ trunk/services/vcs/install.sh
@@ -7,4 +7,4 @@ portsnap extract
 ( cd /usr/ports/devel/subversion ; make -DWITH_APACHE2_APR -DWITHOUT_BDB -DWITH_MOD_DAV_SVN install )
 ( cd /usr/ports/databases/mysql50-server ; make WITH_CHARSET=utf8 WITH_XCHARSET=complex WITH_COLLATION=utf8_general_ci install )
 ( cd /usr/ports/net/libnss-mysql ; make install )
-
+( cd /usr/ports/www/mod_python3 ; make install )
