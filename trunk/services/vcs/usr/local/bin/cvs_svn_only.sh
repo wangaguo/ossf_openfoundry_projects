786ec533bd73f10a144164e32d1d6ee643ed4894
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2007-06-08 03:31:34 +0000

    a restricted shell for cvs and svn only

---
+++ trunk/services/vcs/usr/local/bin/cvs_svn_only.sh
@@ -0,0 +1,16 @@
+#!/bin/sh -
+
+if [ "$1" != "-c" ]; then
+  echo "what????"
+  exit 1
+fi
+if [ "$2" = "cvs server" ]; then
+  exec /usr/bin/cvs server
+fi
+if [ "$2" = "svnserve -t" ]; then
+  exec /usr/local/bin/svnserve -t
+fi
+
+echo "cvs and svn only. no shell access here."
+sleep 5
+exit 1
