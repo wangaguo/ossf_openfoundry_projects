33d9aa05d4996133e794a6354a833a48f0685b5e
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2007-06-08 04:10:30 +0000

    account settings for vcs system map all foundry users to www(80)

--- trunk/services/vcs/usr/local/etc/nss_database.sql
+++ trunk/services/vcs/usr/local/etc/nss_database.sql
@@ -24,68 +24,22 @@ create database auth;
 use auth;
 
 # The tables ...
-CREATE TABLE groups (
-  name varchar(16) NOT NULL default '',
-  password varchar(34) NOT NULL default 'x',
-  gid int(11) NOT NULL auto_increment,
-  PRIMARY KEY  (gid)
-) TYPE=MyISAM AUTO_INCREMENT=5000;
-
-CREATE TABLE grouplist (
-  rowid int(11) NOT NULL auto_increment,
-  gid int(11) NOT NULL default '0',
-  username char(16) NOT NULL default '',
-  PRIMARY KEY  (rowid)
-) TYPE=MyISAM;
 
 CREATE TABLE users (
-  username varchar(16) NOT NULL default '',
-  uid int(11) NOT NULL auto_increment,
-  gid int(11) NOT NULL default '5000',
-  pwchange int(11) NOT NULL default '0',
-  class varchar(64) NOT NULL default '',
-  gecos varchar(128) NOT NULL default '',
-  homedir varchar(255) NOT NULL default '',
-  shell varchar(64) NOT NULL default '/bin/sh',
+  username varchar(16) NOT NULL,
   password varchar(34) NOT NULL default 'x',
-  expire bigint(20) NOT NULL default '0',
-  PRIMARY KEY  (uid),
-  UNIQUE KEY username (username),
-  KEY uid (uid)
-) TYPE=MyISAM AUTO_INCREMENT=5000;
+  PRIMARY KEY  (username),
+  KEY username (username)
+) TYPE=MyISAM;
 
-# The data ...
-INSERT INTO users (username,gecos,homedir,password)
-    VALUES ('cinergi', 'Ben Goodwin', '/home/cinergi', ENCRYPT('cinergi'));
-INSERT INTO groups (name)
-    VALUES ('foobaz');
-INSERT INTO grouplist (gid,username)
-    VALUES (5000,'cinergi');
 
 # The permissions ...
 GRANT USAGE ON *.* TO `nss-root`@`localhost` IDENTIFIED BY 'rootpass';
 GRANT USAGE ON *.* TO `nss-user`@`localhost` IDENTIFIED BY 'userpass';
 
-GRANT Select (`username`, `uid`, `gid`, `gecos`, `homedir`, `shell`,
-              `pwchange`,`class`,`expire`)
+GRANT Select (`username`)
              ON `auth`.`users`
              TO 'nss-user'@'localhost';
-GRANT Select (`username`, `uid`, `gid`, `gecos`, `homedir`, `shell`, `password`,
-              `pwchange`,`class`,`expire`)
+GRANT Select (`username`, `password`)
              ON `auth`.`users`
              TO 'nss-root'@'localhost';
-
-GRANT Select (`name`, `password`, `gid`)
-             ON `auth`.`groups`
-             TO 'nss-user'@'localhost';
-GRANT Select (`name`, `password`, `gid`)
-             ON `auth`.`groups`
-             TO 'nss-root'@'localhost';
-
-GRANT Select (`username`, `gid`)
-             ON `auth`.`grouplist`
-             TO 'nss-user'@'localhost';
-GRANT Select (`username`, `gid`)
-             ON `auth`.`grouplist`
-             TO 'nss-root'@'localhost';
-
