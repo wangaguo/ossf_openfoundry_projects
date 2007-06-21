1b409bc99010b164e205846205281f667fafbe94
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2007-06-21 04:00:50 +0000

    match prefix only

--- trunk/services/vcs/usr/local/bin/cvs_svn_only.sh
+++ trunk/services/vcs/usr/local/bin/cvs_svn_only.sh
@@ -1,12 +1,7 @@
 #!/bin/sh -
 
-if [ "$2" = "cvs server" ]; then
-  exec /usr/bin/cvs server
-fi
-if [ "$2" = "svnserve -t" ]; then
-  exec /usr/local/bin/svnserve -t
-fi
-
-echo "cvs and svn only. no shell access here."
-sleep 5
-exit 1
+case $2 in
+	cvs*) exec /usr/bin/cvs server ;;
+	svnserve*) exec /usr/local/bin/svnserve -t ;;
+	*) echo "cvs and svn only. no shell access here."; sleep 5; exit 1 ;; 
+esac
