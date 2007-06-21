11d50e119bc42f2dc207b4beda7cf4fc5aa26c07
LCamel <LCamel@users.noreply.github.com>
2007-06-21 05:44:43 +0000

    remove debug msg

--- services/vcs/usr/local/bin/openfoundry_sync_nss.pl
+++ services/vcs/usr/local/bin/openfoundry_sync_nss.pl
@@ -6,7 +6,6 @@ use DBI;
 # http://search.cpan.org/~timb/DBI-1.56/DBI.pm
 # http://search.cpan.org/dist/DBD-mysql/lib/DBD/mysql.pm
 
-use Data::Dumper;
 my $of = OpenFoundry->init("RT");
 
 my $dbh = DBI->connect("DBI:mysql:database=$of->{conf}->{NSS_DATABASE}",
@@ -15,12 +14,10 @@ my $dbh = DBI->connect("DBI:mysql:database=$of->{conf}->{NSS_DATABASE}",
                        { RaiseError => 1, AutoCommit => 0 });
 $dbh->do("delete from users");
 my $sth = $dbh->prepare("INSERT INTO users values (?, ?)");
-my $i = 0;
 foreach my $u (@{$of->{'users'}})
 {
 	$sth->execute($u->{Name}, $u->{Password});
 	print ".";
-	die "ouch" if $i++ > 10;
 }
 $dbh->commit();
 $dbh->disconnect();
