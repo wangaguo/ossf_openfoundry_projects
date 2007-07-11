28dd9047bf6ac9647da94dee0791a3c5dd32862a
LCamel <LCamel@users.noreply.github.com>
2007-07-11 08:36:36 +0000

    vcs# perl -e 'exit 1 ? 0 : 1'; echo $? 1 vcs# perl -e 'exit (1 ? 0 : 1)' ; echo $? 0

--- services/vcs/cvs/CVSROOT/commitcheck.pl
+++ services/vcs/cvs/CVSROOT/commitcheck.pl
@@ -11,6 +11,7 @@ if (my ($project) = $ARGV[0] =~ m#^$cvsroot/([^/]+)#)
         exit 1 if not $project;
         exit 1 if $project eq 'CVSROOT';
         my $of = OpenFoundry->init();
-	exit ($of->isInRelationByName("admin", $project, $user) || $of->isInRelationByName("member", $project, $user)) ? 0 : 1;
+	exit(($of->isInRelationByName("admin", $project, $user) || $of->isInRelationByName("member", $project, $user)) ? 0 : 1);
 }
+
 exit 1;
