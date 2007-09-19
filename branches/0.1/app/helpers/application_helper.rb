24c8c18f0532161c04a6e7f0d01c19ce4692e790
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2007-09-19 06:03:24 +0000

    add streamlined options

--- branches/0.1/app/helpers/application_helper.rb
+++ branches/0.1/app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   include Localization
+
+  #for streamlined...
+  def advanced_filtering
+    true
+  end 
+  def breadcrumb
+    true
+  end 
 end
