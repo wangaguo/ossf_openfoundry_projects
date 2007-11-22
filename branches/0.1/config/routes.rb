d500d3b9a7cac25ad64795817d2bf152aebf1d0b
wangaguo <wangaguo@eab794a4-67ee-0310-8774-9eb661efe622>
2007-11-22 06:16:43 +0000

    map to admin

--- branches/0.1/config/routes.rb
+++ branches/0.1/config/routes.rb
@@ -20,4 +20,5 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id.:format'
   map.connect ':controller/:action/:id'
+  map.connect 'admin/:controller/:action/:id', :controller => 'admin/*'
 end
