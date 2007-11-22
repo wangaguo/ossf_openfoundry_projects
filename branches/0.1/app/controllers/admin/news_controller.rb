b5aed76d2e7b444c10219aa0dc236f31c00c7e9b
wangaguo <wangaguo@eab794a4-67ee-0310-8774-9eb661efe622>
2007-11-22 06:30:41 +0000

    remove prop svn:needs-lock

--- branches/0.1/app/controllers/admin/news_controller.rb
+++ branches/0.1/app/controllers/admin/news_controller.rb
@@ -1,9 +1,18 @@
 class Admin::NewsController < AdminController 
   active_scaffold :News do |config|
+    config.show.link.label = '檢視'
+    config.update.link.label = '編輯'
+    config.delete.link.label = '刪除'
+    config.label = 'OpenFoundry 新聞'
+    
     config.columns = [:subject, :descr, :tags, :catid]
     columns[:subject].label = "標題"
     columns[:descr].label = "內容"
     columns[:tags].label = "標籤"
     columns[:catid].label = "專案代碼"
+  end
+
+  def project 
+    @id = params[:id]
   end
 end
