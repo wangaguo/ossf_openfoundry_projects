35f5c4526ce9b8b363182d65db9c7cfe935fde04
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2007-11-26 07:22:11 +0000

    add project admin related UI

--- branches/0.1/app/models/project.rb
+++ branches/0.1/app/models/project.rb
@@ -5,6 +5,10 @@ class Project < ActiveRecord::Base
   PROGRAMMING_LANGUAGES = [ "C", "Java", "Perl", "Ruby" ].freeze
   INTENDED_AUDIENCE = [ "General Use", "Programmer", "System Administrator", "Education", "Researcher" ]
   STATUS = { :APPLYING => 0, :REJECTED => 1, :READY => 2, :SUSPENDED => 3 }
+
+  def self.status_to_s(int_status)
+    STATUS.invert()[int_status]
+  end
   #validates_inclusion_of :license, :in => LICENSES
 
   #support Project-User relationship
@@ -47,15 +51,31 @@ class Project < ActiveRecord::Base
     Project.create(data)
   end
 
-  def approve
-    raise "wrong status! #{self.status}" if self.status != Project::STATUS[:APPLYING]
+  # reason: string
+  def approve(reason)
+    raise "current status is wrong: #{self.status}" if self.status != Project::STATUS[:APPLYING]
     self.status = Project::STATUS[:READY]
+    self.statusreason = reason
     save
   end
   # reason: string
   def reject(reason)
-    raise "wrong status! #{self.status}" if self.status != Project::STATUS[:APPLYING]
+    raise "current status is wrong: #{self.status}" if self.status != Project::STATUS[:APPLYING]
     self.status = Project::STATUS[:REJECTED]
+    self.statusreason = reason
+    save
+  end
+  # reason: string
+  def suspend(reason)
+    raise "current status is wrong: #{self.status}" if self.status != Project::STATUS[:READY]
+    self.status = Project::STATUS[:SUSPENDED]
+    self.statusreason = reason
+    save
+  end
+  # reason: string
+  def resume(reason)
+    raise "current status is wrong: #{self.status}" if self.status != Project::STATUS[:SUSPENDED]
+    self.status = Project::STATUS[:READY]
     self.statusreason = reason
     save
   end
--- branches/0.1/test/unit/project_test.rb
+++ branches/0.1/test/unit/project_test.rb
@@ -24,15 +24,17 @@ class ProjectTest < Test::Unit::TestCase
 
   def test_approve
     p = apply_a_new_project
-    p.approve
+    p.approve("test approve")
 
     p = find_the_new_project
     assert_equal Project::STATUS[:READY], p.status
+    assert_equal "test approve", p.statusreason
   end
   def test_approve_wrong_status
-    p = Project.find(1) # fixture 1 is 'READY'
+    p = Project.find(2) # fixture 2 is 'READY'
+    assert_equal Project::STATUS[:READY], p.status
     assert_raise RuntimeError do
-      p.approve
+      p.approve("test approve")
     end
   end
 
@@ -45,10 +47,45 @@ class ProjectTest < Test::Unit::TestCase
     assert_equal 'test reject', p.statusreason
   end
   def test_reject_wrong_status
-    p = Project.find(1) # fixture 1 is 'READY'
+    p = Project.find(2) # fixture 2 is 'READY'
+    assert_equal Project::STATUS[:READY], p.status
     assert_raise RuntimeError do
       p.reject('test reject')
     end
   end
 
+  def test_suspend
+    p = Project.find(2) # fixture 2 is 'READY'
+    assert_equal Project::STATUS[:READY], p.status
+    p.suspend('test suspend')
+
+    p = Project.find(2)
+    assert_equal Project::STATUS[:SUSPENDED], p.status
+    assert_equal 'test suspend', p.statusreason
+  end
+  def test_reject_wrong_status
+    p = Project.find(1) # fixture 1 is 'APPLYING'
+    assert_equal Project::STATUS[:APPLYING], p.status
+    assert_raise RuntimeError do
+      p.suspend('test suspend')
+    end
+  end
+
+  def test_resume
+    p = Project.find(2) # fixture 2 is 'READY'
+    assert_equal Project::STATUS[:READY], p.status
+    p.suspend('test suspend')
+    p.resume('test resume');
+
+    p = Project.find(2)
+    assert_equal Project::STATUS[:READY], p.status
+    assert_equal 'test resume', p.statusreason
+  end
+  def test_resume_wrong_status
+    p = Project.find(1) # fixture 1 is 'APPLYING'
+    assert_equal Project::STATUS[:APPLYING], p.status
+    assert_raise RuntimeError do
+      p.resume('test suspend')
+    end
+  end
 end
