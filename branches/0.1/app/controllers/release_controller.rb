d8214d49e11bc5fc387f46676239fbf5c83e5f55
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2007-11-29 03:48:26 +0000

    finish UPLOAD FILES ACTION

--- branches/0.1/app/controllers/release_controller.rb
+++ branches/0.1/app/controllers/release_controller.rb
@@ -13,7 +13,7 @@ class ReleaseController < ApplicationController
   #list all release for a given :project_id
   def list
     @releases = Release.find :all,
-      :conditions => "project_id = #{params[:id]}"
+      :conditions => "project_id = #{params[:project_id]}"
   end
   
   def new
@@ -39,7 +39,7 @@ class ReleaseController < ApplicationController
       #update!
     end
   end
-
+  
   def uploadfiles
     pattern = params[:move]
     pattern ||= '/tmp'
@@ -48,7 +48,9 @@ class ReleaseController < ApplicationController
     #加上File match mark "**", see File:fnmatch, Dir.glob
     pattern = File.join pattern,'**'
     
-    #把目錄下的所有東西撈出來, 目錄以'd'表示, 檔案用'f'
+    @release = Release.find params[:release_id]
+    @project = Project.find params[:project_id]
+    
     @uploadfiles = []
     @uploaddirs = []
     Dir.glob(pattern){ |file|
@@ -66,7 +68,33 @@ class ReleaseController < ApplicationController
     
     #不套用layout
     if params[:layout] == 'false'
-      render :layout => false
+      render :layout => false   
+    end
+  end
+  
+  def addfiles
+    #if request.post?
+      r = Release.find params[:release_id]
+      return if r.nil?
+      files = params[:uploadfiles].collect { |path| make_file_entity path }
+      r.fileentity << files
+      r.save
+      flash.now[:message] = 'Your files have been added to Release!'
+      
+      uploadfiles
+      render :layout =>false, :action => :uploadfiles
+    #else
+      #TODO wrong argument!
+    #end
+  end
+  
+  private
+  def make_file_entity(path)
+    unless ret=Fileentity.find_by_path(path).nil?
+      ret
+    else
+      #TODO collect meta info for FILE
+      Fileentity.create ( :attributes => {:path => path} )
     end
   end
   
--- branches/0.1/app/helpers/release_helper.rb
+++ branches/0.1/app/helpers/release_helper.rb
@@ -1,2 +1,18 @@
 module ReleaseHelper
+  def show_release(release)
+    html=''
+    html << '<fieldset><dl>'
+    for column in Release.content_columns 
+      html << "<dt>#{column.human_name}:</dt><dd>#{release.send(column.name)}</dd>"
+    end
+    html << '</dl>'
+    release.fileentity.each do |f|
+      for column in Fileentity.content_columns
+        html << "<dt>#{column.human_name}:</dt><dd>#{f.send(column.name)}</dd>"
+      end
+      html << '</dl>'
+    end
+    html << '</fieldset>'
+    html
+  end
 end
