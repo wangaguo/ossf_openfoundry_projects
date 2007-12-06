6f04ffc6bd48c7e3c2c1e34f84f42610f566252e
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2007-12-06 05:56:05 +0000

    modify releases routes

--- branches/0.1/config/routes.rb
+++ branches/0.1/config/routes.rb
@@ -25,17 +25,18 @@ ActionController::Routing::Routes.draw do |map|
   #for project releases
   map.resources :releases,
     :path_prefix => '/projects/:project_id',
-    :member => { :uploadfiles => :any, :delete => :post, :addfiles => :post },
+    :member => { :uploadfiles => :any, :delete => :post, 
+                 :addfiles => :post, :removefile => :post },
     :singular => :release
   
-#  map.release 'project/:project_id/release', 
-#    :controller => 'release', :action => 'list'
-#  map.release 'project/:project_id/release/:release_id',
-#    :controller => 'release', :action => 'show'
-#  map.release 'project/:project_id/release/:action', 
-#    :controller => 'release'
-#  map.release 'project/:project_id/release/:action/:release_id', 
-#    :controller => 'release'
+  #  map.release 'project/:project_id/release', 
+  #    :controller => 'release', :action => 'list'
+  #  map.release 'project/:project_id/release/:release_id',
+  #    :controller => 'release', :action => 'show'
+  #  map.release 'project/:project_id/release/:action', 
+  #    :controller => 'release'
+  #  map.release 'project/:project_id/release/:action/:release_id', 
+  #    :controller => 'release'
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
