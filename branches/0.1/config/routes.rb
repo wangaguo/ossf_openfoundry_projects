0b1fab4535d287a3a90985ad6222faf550bc8487
thkuo <thkuo@eab794a4-67ee-0310-8774-9eb661efe622>
2007-12-18 03:45:09 +0000

    add download routes

--- branches/0.1/config/routes.rb
+++ branches/0.1/config/routes.rb
@@ -48,8 +48,10 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   
   #for download area~
-  map.connect 'download_path/:project_name/:file_name', :controller => 'openfoundry',
-       :action => 'download'
+  map.connect 'download_path/:project_name/:file_name',
+    :controller => 'openfoundry',
+    :action => 'download',
+    :requirements => {:file_name => /.+/}
 #  require "pp"
 #  pp map.instance_eval("@set").instance_eval("@named_routes").instance_eval("@helpers").map {|x| x.to_s}.grep(/url/).select {|x| not x=~/^(hash|formatted)/}
   
