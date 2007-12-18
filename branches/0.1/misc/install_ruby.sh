018f21bd9d6250bef054d45c395f02c9f0a7356a
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2007-12-18 07:43:13 +0000

    for newer versions of rails

--- branches/0.1/misc/install_ruby.sh
+++ branches/0.1/misc/install_ruby.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 myprefix=$HOME/ruby
-sudo apt-get install build-essential zlib1g-dev
+sudo apt-get install build-essential zlib1g-dev libreadline-dev libssl-dev
 
 mkdir -p $myprefix
 mkdir -p $myprefix/download
 cd $myprefix/download
 
-wget ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.6-p110.tar.gz
-tar zxpf ruby-1.8.6-p110.tar.gz
-cd ruby-1.8.6-p110
+wget ftp://ftp.ruby-lang.org/pub/ruby/1.8/ruby-1.8.6-p111.tar.gz
+tar zxpf ruby-1.8.6-p111.tar.gz
+cd ruby-1.8.6-p111
 ./configure --prefix "$myprefix/ruby"
 make
-make install
+make install-all
 PATH="$myprefix/ruby/bin:$PATH"; export PATH
 cd ..
 
@@ -20,6 +20,7 @@ tar zxpf rubygems-0.9.4.tgz
 cd rubygems-0.9.4
 GEM_HOME="$myprefix/gem_home"; export GEM_HOME
 ruby setup.rb config --prefix="$myprefix/gem_prefix"
+#ruby setup.rb config
 ruby setup.rb setup
 ruby setup.rb install
 cd ..
@@ -28,3 +29,4 @@ cd ..
 echo "Please add the following settings:"
 echo "GEM_HOME=\"$myprefix/gem_home\"; export GEM_HOME"
 echo "PATH=\"$myprefix/ruby/bin:$myprefix/gem_prefix/bin:$myprefix/gem_home/bin:\$PATH\"; export PATH"
+#echo "PATH=\"$myprefix/ruby/bin:\$PATH\"; export PATH"
