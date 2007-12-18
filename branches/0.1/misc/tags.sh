07b5d70604139489a98e1625aec3b0af8a44f057
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2007-12-18 07:48:16 +0000

    for newer versions of ruby

--- branches/0.1/misc/tags.sh
+++ branches/0.1/misc/tags.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir ~/tags
-ctags -f ~/tags/ruby_c -V -R --languages=-Ruby ~/ruby/download/ruby-1.8.6-p110
-rtags -f ~/tags/ruby_ruby --vi -R  ~/ruby/download/ruby-1.8.6-p110
+ctags -f ~/tags/ruby_c -V -R --languages=-Ruby ~/ruby/download/ruby-1*
+rtags -f ~/tags/ruby_ruby --vi -R  ~/ruby/ruby
 rtags -f ~/tags/ruby_gems --vi -R  ~/ruby/gem_home
 rtags -f ~/tags/0.1 --vi -R  ~/checkout/branches/0.1
 
