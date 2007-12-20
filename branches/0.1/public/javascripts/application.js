2fdbdba93e2d90c97a421d8d7acde957d0db4dd0
wangaguo <wangaguo@eab794a4-67ee-0310-8774-9eb661efe622>
2007-12-20 02:40:44 +0000

    set language

--- branches/0.1/public/javascripts/application.js
+++ branches/0.1/public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function SetLang(lang)
+{
+  var langHref = "";
+  langHref = location.search.replace(/[&|?]lang=[^&]*/,'').replace(/^[&|?]*/,'');
+  if(langHref == "") langHref += "?";
+  else langHref = "?" + langHref + "&";
+  location.href =  location.href.replace(/\?.*/,'') + langHref + "lang=" + lang;
+}
