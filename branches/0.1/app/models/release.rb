d39cae4155540f8a0b6483bfe36cf91f2345dfae
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2007-12-21 07:41:41 +0000

    add build_path hook

--- branches/0.1/app/models/release.rb
+++ branches/0.1/app/models/release.rb
@@ -1,4 +1,18 @@
 class Release < ActiveRecord::Base
   belongs_to :project
   has_many :fileentity
+  
+  def self.build_path(project_name)
+    #for ftp upload "prefix/project_name"
+    prefix = Project::PROJECT_UPLOAD_PATH
+    Dir.mkdir(prefix) unless File.directory?(prefix)
+    prefix = File.join(prefix, project_name)
+    Dir.mkdir(prefix) unless File.directory?(prefix)
+    
+    #for web download "prefix/project_name"
+    prefix = Project::PROJECT_DOWNLOAD_PATH
+    Dir.mkdir(prefix) unless File.directory?(prefix)
+    prefix = File.join(prefix, project_name)
+    Dir.mkdir(prefix) unless File.directory?(prefix)    
+  end
 end
