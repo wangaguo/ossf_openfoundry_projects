af59b12c280e860ff37c169023f6eb8c08f83d9b
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2007-12-25 08:27:25 +0000

    check site_admin permission

--- branches/0.1/app/controllers/site_admin.rb
+++ branches/0.1/app/controllers/site_admin.rb
@@ -1,3 +1,8 @@
 class SiteAdmin < ApplicationController 
   layout "admin"
+  before_filter :check_site_admin
+
+  def check_site_admin
+    redirect_to :controller => '../openfoundry' if not current_user().has_role?('site_admin')
+  end
 end
