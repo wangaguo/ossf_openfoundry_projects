d24645ba2c185707b604f49fc6cba6d61d0aac6e
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2008-01-02 09:10:59 +0000

    debugger settings for rails 2.0

--- branches/0.1/config/environments/development.rb
+++ branches/0.1/config/environments/development.rb
@@ -8,9 +8,6 @@ config.cache_classes = false
 # Log error messages when you accidentally call methods on nil.
 config.whiny_nils = true
 
-# Enable the breakpoint server that script/breakpointer connects to
-config.breakpoint_server = true
-
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
 config.action_controller.perform_caching             = false
@@ -19,3 +16,7 @@ config.action_view.debug_rjs                         = true
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+# debugging
+# http://www.datanoise.com/articles/2006/07/12/tutorial-on-ruby-debug
+#require 'ruby-debug'
