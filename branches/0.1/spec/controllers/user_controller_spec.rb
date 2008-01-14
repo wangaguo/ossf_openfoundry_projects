cd78851724e1b9e5a8d8e6a09b57d51e834343b9
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-01-14 03:40:07 +0000

    create dir for rspec and releases

---
+++ branches/0.1/spec/controllers/user_controller_spec.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe UserController, "when signup" do
+
+  it "should send notification email" do
+    pending
+  end
+
+  it "should redirect to login" do
+    pending
+  end
+
+end
+
+describe UserController, "after user auth" do
+  it "could edit user preferance" do
+    pending
+  end
+
+  it "" do
+    pending
+  end
+end
+
