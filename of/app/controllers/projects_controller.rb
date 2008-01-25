e2af629274b2fbf23ef9fe8193c1118c080db2f1
wangaguo <wangaguo@gmail.com>
2008-01-25 12:57:37 +0000

    roles & users display in Project info

--- of/app/controllers/projects_controller.rb
+++ of/app/controllers/projects_controller.rb
@@ -32,8 +32,7 @@ class ProjectsController < ApplicationController
 
   def show
     @project = Project.find(params[:id])
-    @admins = @project.admins
-    @members = @project.members
+    @roles = @project.roles
   end
 
   def new
