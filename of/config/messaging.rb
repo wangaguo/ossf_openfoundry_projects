f04f50c9442245d39ab9840487796e04e2343e9b
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-03-11 03:46:48 +0000

    add activemessaging and mq support

---
+++ of/config/messaging.rb
@@ -0,0 +1,13 @@
+#
+# Add your destination definitions here
+# can also be used to configure filters, and processor groups
+#
+ActiveMessaging::Gateway.define do |s|
+  #s.destination :orders, '/queue/Orders'
+  #s.filter :some_filter, :only=>:orders
+  #s.processor_group :group1, :order_processor
+  
+  s.destination :message_client, '/queue/MessageClient'
+  
+  s.destination :ossf_msg, '/topic/OSSF.MSG'
+end
