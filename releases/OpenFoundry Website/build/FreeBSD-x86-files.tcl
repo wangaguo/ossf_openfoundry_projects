7b7b9671453e4760ffb6aeabbe928662602c145c
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-04-07 08:23:00 +0000

    add Website and VCS installer, update host installer

---
+++ releases/OpenFoundry Website/build/FreeBSD-x86-files.tcl
@@ -0,0 +1,4 @@
+proc ::InstallJammer::InitFiles {} {
+    File ::731E883A-74C8-A30C-896E-FD35956467EC -name install.sh -parent 067E12AE-14EE-C5A3-BC08-0BCB563B8C02 -directory <%InstallDir%> -size 1286 -mtime 1205113648 -permissions 00755 -filemethod "Update files with more recent dates"
+
+}
