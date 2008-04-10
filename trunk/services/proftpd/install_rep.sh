776664a357145650ce54263dfa6389c789b88443
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-04-10 09:24:13 +0000

    remove debug msg, add BATCH=yes when make proftpd

--- trunk/services/proftpd/install_rep.sh
+++ trunk/services/proftpd/install_rep.sh
@@ -10,7 +10,7 @@ export MASTER_SITE_OVERRIDE
 
 ( cd /usr/ports/www/apache22 ; make BATCH=yes install )
 ( cd /usr/ports/databases/mysql50-client ; make WITH_CHARSET=utf8 WITH_XCHARSET=complex WITH_COLLATION=utf8_general_ci install )
-( cd /usr/ports/ftp/proftpd; make WITH_OPENSSL=true WITH_QUOTA=true WITH_MYSQL=true WITH_NLS=true install clean distclean )
+( cd /usr/ports/ftp/proftpd; make BATCH=yes WITH_OPENSSL=true WITH_QUOTA=true WITH_MYSQL=true WITH_NLS=true install clean distclean )
 
 /usr/local/etc/rc.d/apache22 rcvar | grep YES || echo 'apache22_enable="YES"' >> /etc/rc.conf
 /usr/local/etc/rc.d/apache22 start
