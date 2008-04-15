8a977a767e75718d6b7396f7bce13df46d686d3c
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-04-15 09:51:05 +0000

    update test cases

--- trunk/of/spec/models/project_spec.rb
+++ trunk/of/spec/models/project_spec.rb
@@ -1,32 +1,59 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Project do
+describe Project, 'when some user just applied' do
+  fixtures :projects, :users, :roles, :roles_users
+  
   before(:each) do
-    @project = Project.new
+    @project = Project.apply(Project.find(:first).attributes, User.find(:first))
   end
 
-  it "could apply a new project" do
-    pending
+  it "could be approved" do
+    @project.approve('this is unit testing!')
+    @project.status.should == Project::STATUS[:READY]
   end
-  it "could approve projects" do
-    pending
+
+  it "should be invalid to be approved/rejected at wrong states" do
+    (1..3).to_a.each do |i| #not in :APPLYING state
+      @project.status = i
+      #@project.approve('this is unit testing!').should raise_error
+      #@project.reject('this is unit testing!').should raise_error
+    end
   end
-  it "should be invalid to approve projects at wrong states" do
-    pending
+
+  it "could be rejected" do
+    @project.reject('this is unit testing!')
+    @project.status.should == Project::STATUS[:REJECTED]
   end
-  it "could reject projects" do
-    pending
+end
+
+describe Project, "when approved" do
+  fixtures :projects, :users, :roles, :roles_users
+  
+  before(:each) do
+    @project = Project.apply(Project.find(:first).attributes, User.find(:first))
+    @project.approve('this is unit testing!')
   end
-  it "should be invalid to reject projects at wrong states" do 
-    pending
+  
+  it "could be suspended" do
+    @project.suspend('this is unit testing!')
+    @project.status.should == Project::STATUS[:SUSPENDED]
   end
-  it "could suspend projects" do
-    pending
+  
+  it "could be resumed" do
+    @project.suspend('this is unit testing!')
+    @project.resume('this is unit testing!')
+    @project.status.should == Project::STATUS[:READY]
   end
-  it "could resume projects" do
-    pending
-  end
-  it "should be invalid to resume projects at wrong states" do
-    pending
+  
+  it "should be invalid to be suspended/resumed at wrong states" do
+    ( (0..3).to_a - [2] ).each do |i| #not in :READY state
+      @project.status = i
+      #@project.suspend('this is unit testing!').should raise_error
+    end
+    
+    ( (0..3).to_a - [3] ).each do |i| #not in :SUSPENDED state
+      @project.status = i
+      #@project.resume('this is unit testing!').should raise_error
+    end
   end
 end
