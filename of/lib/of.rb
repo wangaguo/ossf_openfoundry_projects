90fb92685b72ff3e67faed28cb1a35117976ed0d
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-05-07 03:23:09 +0000

    update message module

--- of/lib/of.rb
+++ of/lib/of.rb
@@ -3,6 +3,10 @@ require 'yaml'
 
 module OpenFoundry
   module Message
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+
     # for deliver ACTIONS, like CRUD
     ACTIONS = {
       :create => 'create', 
@@ -14,16 +18,19 @@ module OpenFoundry
       :project => 'project', 
       :user => 'user' , 
       :relation => 'relation'}.freeze
-    
-    include ActiveMessaging::MessageSender
-    
-    publishes_to :ossf_msg
-    
-    def send_msg(type, action, data)
-      publish(:ossf_msg, 
-        "#{YAML::dump({'type' => type, 'action' => action, 'data' => data})}"
-      )
+
+    module ClassMethods
+      include ActiveMessaging::MessageSender
+      
+      publishes_to :ossf_msg
+     
+      def send_msg(type, action, data)
+        publish(:ossf_msg, 
+          "#{YAML::dump({'type' => type, 'action' => action, 'data' => data})}"
+        )
+      end
     end
-    
   end
 end
+
+ActiveRecord::Base.send(:include, OpenFoundry::Message)
