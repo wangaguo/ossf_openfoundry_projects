a403a1cc532662f5648103f9158afc741139f64f
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-05-08 07:54:38 +0000

    update site_admin view

--- of/app/controllers/site_admin/user_controller.rb
+++ of/app/controllers/site_admin/user_controller.rb
@@ -1,3 +1,6 @@
 class SiteAdmin::UserController < SiteAdmin
-  active_scaffold :User
+  active_scaffold :User do |config| 
+    config.list.columns = [:login, :verified, :status, :created_at, :updated_at]
+    config.columns = [:login, :verified, :status, :created_at, :updated_at]
+  end
 end
