86a733b43f671b2977deeab0f21e87d1de8ff9a1
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-05-13 08:44:43 +0000

    bug fixed

--- trunk/of/db/migrate/002_add_site_data.rb
+++ trunk/of/db/migrate/002_add_site_data.rb
@@ -8,42 +8,42 @@ class AddSiteData < ActiveRecord::Migration
     end
     desc = ['Create a New Role', 'Edit Role Permissions', 'Delete Roles']
     #Role
-    %w(new edit delete).each do |name|
+    %w(new edit delete).each_with_index do |name, i|
       Function.create :name => "role_#{name}", :module => 'Role', 
         :description => desc[i]
     end
     
     #Relesae
     desc = ['Add new Release', 'Edit Releases', 'Delete Release', 'View Release Details']
-    %w(new edit delete view).each do |name|
+    %w(new edit delete view).each_with_index do |name, i|
       Function.create :name => "release_#{name}", :module => 'Release', 
         :description => desc[i]
     end
     
     #News
     desc = ['Post new Release', 'Edit News', 'Delete News', 'View News Details']
-    %w(post edit delete view).each do |name|
+    %w(post edit delete view).each_with_index do |name, i|
       Function.create :name => "news_#{name}", :module => 'News', 
         :description => desc[i]
     end
     
     #Wiki
     desc = ['Add Wiki Page', 'Edit Wiki Pages', 'Delete Wiki Page', 'View Page Details']
-    %w(new edit delete view).each do |name|
+    %w(new edit delete view).each_with_index do |name, i|
       Function.create :name => "wiki_#{name}", :module => 'Wiki', 
         :description => desc[i]
     end
     
     #forums
     desc = ['Post Messages', 'View List Details', 'Access Message Archives']
-    %w(post view archive).each do |name|
+    %w(post view archive).each_with_index do |name, i|
       Function.create :name => "forums_#{name}", :module => 'Forums', 
         :description => desc[i]
     end
     
     #VCS
     desc = ['do SVN import', 'do CVS import', 'do SVN commit', 'do CVS commit']
-    %w(svn_import cvs_import svn_ci cvs_ci).each do |name|
+    %w(svn_import cvs_import svn_ci cvs_ci).each_with_index do |name, i|
       Function.create :name => "vcs_#{name}", :module => 'Vcs', 
         :description => desc[i]
     end
