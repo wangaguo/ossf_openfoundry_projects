c3bd6ddfb73d669061559ae417ef69bb9e41ea89
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-06-16 06:27:40 +0000

    update vcs install/config script

--- services/vcs/install.sh
+++ services/vcs/install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-OPENFOUNDRY_HOME=/usr/local/openfoundry
+OPENFOUNDRY_HOME=/usr/local/vcs
 OPENFOUNDRY_CHECKOUT="${OPENFOUNDRY_HOME}/checkout"
 OPENFOUNDRY_ETC="${OPENFOUNDRY_HOME}/etc"
+OPENFOUNDRY_PM_CHECKOUT="${OPENFOUNDRY_CHECKOUT}/module"
+VCS_CHECKOUT="${OPENFOUNDRY_CHECKOUT}/vcs"
 
 date
 
@@ -30,13 +32,14 @@ cp /usr/local/etc/apache22/httpd.conf /root/httpd.conf.dist
 ( cd /usr/ports/devel/subversion ; make BATCH=yes -DWITH_APACHE2_APR -DWITHOUT_BDB -DWITH_MOD_DAV_SVN -DWITH_PYTHON install )
 cp /usr/local/etc/apache22/httpd.conf /root/httpd.conf.subversion
 
-svn co http://svn.openfoundry.org/openfoundry "${OPENFOUNDRY_CHECKOUT}"
+svn co http://svn.openfoundry.org/openfoundry/trunk/services/vcs "${VCS_CHECKOUT}"
+svn co http://svn.openfoundry.org/openfoundry/trunk/openfoundry "${OPENFOUNDRY_PM_CHECKOUT}"
 
 ( cd /usr/ports/databases/mysql50-server ; make WITH_CHARSET=utf8 WITH_XCHARSET=complex WITH_COLLATION=utf8_general_ci install )
 
 ( cd /usr/ports/net/libnss-mysql ; make install )
 
-( cd /usr/ports/security/pwauth ; make patch ; patch $WRKDIRPREFIX/usr/ports/security/pwauth/work/pwauth-*/config.h < "${OPENFOUNDRY_CHECKOUT}/trunk/services/vcs/usr/ports/security/pwauth/config.h.diff" ; make install )
+( cd /usr/ports/security/pwauth ; make patch ; patch $WRKDIRPREFIX/usr/ports/security/pwauth/work/pwauth-*/config.h < "${OPENFOUNDRY_CHECKOUT}/usr/ports/security/pwauth/config.h.diff" ; make install )
 
 ( cd /usr/ports/www/mod_authnz_external ; make install )
 cp /usr/local/etc/apache22/httpd.conf /root/httpd.conf.mod_authnz_external
@@ -50,20 +53,20 @@ cp /usr/local/etc/apache22/httpd.conf /root/httpd.conf.mod_authnz_external
 ( cd /usr/ports/databases/p5-DBD-mysql50 ; make install )
 
 
-csup -g -L 2 "${OPENFOUNDRY_CHECKOUT}/trunk/services/vcs/usr/src/for-cvs-supfile"
-patch /usr/src/contrib/cvs/src/server.c < "${OPENFOUNDRY_CHECKOUT}/trunk/services/vcs/usr/src/contrib/cvs/src/server.c.diff"
+csup -g -L 2 "${OPENFOUNDRY_CHECKOUT}/usr/src/for-cvs-supfile"
+patch /usr/src/contrib/cvs/src/server.c < "${OPENFOUNDRY_CHECKOUT}/usr/src/contrib/cvs/src/server.c.diff"
 ( cd /usr/src/gnu/usr.bin/cvs ; make )
 install -s -o root -g wheel -m 555 -b /usr/src/gnu/usr.bin/cvs/cvs/cvs /usr/bin
 
 
 mkdir -p "${OPENFOUNDRY_ETC}"
-ln -sf "${OPENFOUNDRY_CHECKOUT}/trunk/services/vcs/usr/local/openfoundry/etc/openfoundry.conf.dist" "${OPENFOUNDRY_ETC}"
-ln -sf "${OPENFOUNDRY_CHECKOUT}/trunk/services/vcs/usr/local/openfoundry/etc/openfoundry_root.conf.dist" "${OPENFOUNDRY_ETC}"
+ln -sf "${VCS_CHECKOUT}/usr/local/openfoundry/etc/openfoundry.conf.dist" "${OPENFOUNDRY_ETC}"
+ln -sf "${VCS_CHECKOUT}/usr/local/openfoundry/etc/openfoundry_root.conf.dist" "${OPENFOUNDRY_ETC}"
 cp "${OPENFOUNDRY_ETC}/openfoundry.conf.dist" "${OPENFOUNDRY_ETC}/openfoundry.conf"
 cp "${OPENFOUNDRY_ETC}/openfoundry_root.conf.dist" "${OPENFOUNDRY_ETC}/openfoundry_root.conf"
 chmod 600 "${OPENFOUNDRY_ETC}/openfoundry_root.conf"
 
-fetch -o /root/config.sh http://svn.openfoundry.org/openfoundry/trunk/services/vcs/config.sh
+#fetch -o /root/config.sh http://svn.openfoundry.org/openfoundry/trunk/services/vcs/config.sh
 
 
 echo "*************************************************************************************************"
