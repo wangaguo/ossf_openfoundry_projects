6d1b07c5e71f20aeb95d0bcd3b82777a025e8c5f
LCamel <LCamel@users.noreply.github.com>
2008-06-23 05:21:37 +0000

    extract "value" variable and sort values in of_check_box_group

--- of/app/helpers/projects_helper.rb
+++ of/app/helpers/projects_helper.rb
@@ -1,11 +1,12 @@
 module ProjectsHelper
   def of_check_box_group(name, predefined, value)
-    value ||= ""
+    values = "#{value}".split(",")
     rtn = ""
+    predefined = predefined.sort
     predefined.each_with_index do |p, i|
-      rtn += check_box_tag("#{name}[#{i}]", p, value.split(",").include?(p)) + p + "\n"
+      rtn += check_box_tag("#{name}[#{i}]", p, values.include?(p)) + p + "\n"
     end
     rtn += "<br/> Others "
-    rtn += text_field_tag("#{name}[-1]", (value.split(",") - predefined).join(","))
+    rtn += text_field_tag("#{name}[-1]", (values - predefined).join(","))
   end
 end 
