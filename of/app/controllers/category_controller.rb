a66ac48c76d87f022144d783ecfa60e9e2d52226
wangaguo <wangaguo@gmail.com>
2008-06-23 07:48:29 +0000

    add project category

--- of/app/controllers/category_controller.rb
+++ of/app/controllers/category_controller.rb
@@ -11,6 +11,30 @@ class CategoryController < ApplicationController
 	render :layout => false
   end
 
+  def index
+    list
+    render :template => 'category/list'
+  end
+  
+  def list
+    projects = Project.find(:all)
+#    @maturity = []
+#    @license = []
+#    @content_license = []
+    @platform = []
+    @programming_language = []
+    @intended_audience = []
+    
+    projects.each do |p|
+#      @maturity |= p.maturity.split(",")
+#      @license |= p.license.split(",")
+#      @content_license |= p.contentlicense.split(",")
+      @platform |= p.platform.split(",")
+      @programming_language |= p.programminglanguage.split(",")
+      @intended_audience |= p.intendedaudience.split(",")      
+    end
+  end
+
   def show
   end
 
