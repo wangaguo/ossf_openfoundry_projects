61f5a4108fdc6455c79781641e7ee6e08c79cbd4
wangaguo <wangaguo@gmail.com>
2008-06-26 04:05:51 +0000

    News: 1.Add status constant 2.save lost status field 3.validate status field

--- of/app/models/news.rb
+++ of/app/models/news.rb
@@ -1,7 +1,9 @@
 class News < ActiveRecord::Base
+  STATUS = {:Enabled => 1, :Disabled => 0}
+  
   validates_length_of :subject, :within => 3..100, :too_long => _("Length range is ") + "3-100", :too_short => _("Length range is ") + "3-100"
   validates_length_of :description, :within => 3..4000, :too_long => _("Length range is ") + "3-4000", :too_short => _("Length range is ") + "3-4000"
-  validates_numericality_of :status, :less_than_or_equal_to => 1, :message => _("Not a valid value")
+  validates_inclusion_of :status, :in => STATUS.values, :message => _("Not a valid value")
   validates_date_time :updated_at, :message => _("Not a valid date time"), :allow_nil => true
   
   #add fulltext indexed SEARCH
