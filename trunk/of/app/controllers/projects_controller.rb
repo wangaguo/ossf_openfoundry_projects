14105cb5a94f10d7d8c128380d86d1f9c53bb634
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2008-07-03 08:31:59 +0000

    project: show only in used projects

--- trunk/of/app/controllers/projects_controller.rb
+++ trunk/of/app/controllers/projects_controller.rb
@@ -12,13 +12,16 @@ class ProjectsController < ApplicationController
     rtn = nil
     case id_or_name
     when /^\d+$/
-      rtn = Project.find_by_id(id_or_name)
+      rtn = Project.find_by_id(id_or_name, :conditions => Project.in_used_projects())
     when Project::NAME_REGEX
-      if rtn = Project.find(:first, :select => 'id', :conditions => ["name = ?", id_or_name])
+      if rtn = Project.find(:first, :select => 'id', :conditions => Project.in_used_projects(["name = ?", id_or_name]))
         yield rtn.id
       end
     end
-    controller.send(:redirect_to, "/") if not rtn # ....
+    if not rtn
+      controller.send(:flash)[:warning] = "Project '#{id_or_name}' does not exist, or it has be deactivated."
+      controller.send(:redirect_to, "/")
+    end
     rtn
   end
 
