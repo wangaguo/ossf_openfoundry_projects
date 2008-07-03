d6eca78a3a0b4a9b8b4d19857fd6b704f5425f53
wangaguo <wangaguo@gmail.com>
2008-07-03 13:40:41 +0000

    Job(Help Wanted):add job module

---
+++ of/app/controllers/jobs_controller.rb
@@ -0,0 +1,78 @@
+class JobsController < ApplicationController 
+  layout 'module'
+  find_resources :parent => 'project', :child => 'job', :parent_id_method => 'project_id', :child_rename => 'data_item'
+  before_filter :controller_load
+  before_filter :check_permission
+
+  def controller_load
+    @module_name = _('Help Wanted')
+  end
+  
+  def index
+    list
+  end
+
+  def list
+    conditions = "project_id=" + params[:project_id]
+    if @has_permit = fpermit?("job", params[:project_id])
+      sqlStatus = ''
+    else
+      sqlStatus = ' and status = "' + Job::STATUS[:Enabled].to_s + '"'
+    end
+    reset_sortable_columns
+    add_to_sortable_columns('listing', Job, 'subject', 'subject') 
+    add_to_sortable_columns('listing', Job, 'updated_at', 'updated_at') 
+    @data_items = Job.paginate(:page => params[:page], :per_page => 10, :conditions => [conditions + sqlStatus],
+                          :order => sortable_order('listing', :model => Job, :field => 'updated_at', :sort_direction => :desc) )
+    render :template => 'jobs/list'
+  end
+  
+  def show
+    @data_item.due = @data_item.due.strftime("%Y-%m-%d") if !@data_item.due.nil?
+  end
+  
+  def new
+    @data_item = Job.new
+  end
+  
+  def create
+    @data_item = Job.new
+    @data_item.subject = params[:data_item][:subject]
+    @data_item.description = params[:data_item][:description]
+    @data_item.requirement = params[:data_item][:requirement]
+    @data_item.due = params[:data_item][:due]
+    @data_item.project_id = params[:project_id]
+    @data_item.status = params[:data_item][:status]
+    if @data_item.save
+      flash[:notice] = _('Add Successful')
+      redirect_to :action => 'index'
+    else
+      render :action => 'new'
+    end
+  end
+  
+  def edit
+    @data_item.due = @data_item.due.strftime("%Y-%m-%d") if !@data_item.due.nil?
+  end
+  
+  def update
+    @data_item.subject = params[:data_item][:subject]
+    @data_item.description = params[:data_item][:description]
+    @data_item.requirement = params[:data_item][:requirement]
+    @data_item.due = params[:data_item][:due]
+    @data_item.status = params[:data_item][:status]
+    if @data_item.save
+      flash[:notice] = _('Edit Successful')
+      redirect_to :action => 'show', :id => @data_item
+    else
+      render :action => 'edit'
+    end
+  end
+  
+  def destroy
+    @data_item.destroy
+    flash[:notice] = _('Delete Successful')
+    redirect_to :action => 'index'
+  end
+
+end
