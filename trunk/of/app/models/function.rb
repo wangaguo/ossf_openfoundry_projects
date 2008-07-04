7706f798f41d5a7dba3f449b86d5d265a45fc2a2
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-07-04 01:54:07 +0000

    permission: bug fix for fpermit?

--- trunk/of/app/models/function.rb
+++ trunk/of/app/models/function.rb
@@ -7,22 +7,29 @@ class Function < ActiveRecord::Base
     #if the permission is allow_all, allow it whoever 
     return true if function_name.to_s == 'allow_all'
 
-    #if user the the admin of this project, allow it anyway
-    return true if Role.count_by_sql("select U.id from roles R, roles_users RU, users U 
+    #if user is the admin of this project, allow it anyway
+    return true if Role.count_by_sql(
+      "select U.id from roles R, roles_users RU, users U, projects P  
         where U.id = RU.user_id and R.id = RU.role_id and R.name = 'Admin' and
               R.authorizable_id = '#{authorizable_id}' and 
-              R.authorizable_type = 'Project' and
-              U.login = '#{current_user.login}'                    
+              R.authorizable_type = 'Project' and 
+              R.authorizable_id  = P.id and 
+              #{Project.in_used_projects('true', :alias => 'P')} and 
+              U.login = '#{current_user.login}' and 
+              #{User.verified_users('true', :alias => 'U')}                      
         ") > 0
     
     #else check every permission carefully!
     if(0 < Function.count_by_sql("" +
-        "select count(*) from roles, roles_users, users, roles_functions, functions " +
+        "select count(*) from roles, roles_users, users, roles_functions, functions, projects " +
         "where roles_users.user_id = users.id and roles_users.role_id = roles.id and " +
         "roles_functions.role_id = roles.id and roles_functions.function_id = functions.id and " +
         "roles.authorizable_type = '#{authorizable_type}' and " + 
         "roles.authorizable_id = '#{authorizable_id}' and " +
+        "roles.authorizable_id = projects.id and " +
+        "#{Project.in_used_projects('true', :alias => 'projects')} and " +
         "users.login = '#{current_user.login}' and " + 
+        "#{User.verified_users('true', :alias => 'user')} and " +
         "functions.name = '#{function_name}'"))
       return true
     else
