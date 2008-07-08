7a6b346a412e13de5b24e2aa330fbd8615eadd56
wangaguo <wangaguo@gmail.com>
2008-07-08 02:02:47 +0000

    jobs:blongs_to project

--- of/app/models/job.rb
+++ of/app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ActiveRecord::Base
+  belongs_to :project, :foreign_key => "project_id"
   STATUS = {:Enabled => 1, :Disabled => 0}
 
   validates_length_of :subject, :within => 3..255, :too_long => _("Length range is ") + "3-255", :too_short => _("Length range is ") + "3-255"
@@ -11,4 +12,10 @@ class Job < ActiveRecord::Base
     due.nil? || date_pattern !~ due_before_type_cast
   end
   
+  def self.AllProjectLatestJobs
+    Job.find(:all, :joins => :project, 
+              :conditions => ["jobs.due > getdate() and jobs.status = #{Job::STATUS[:Enabled]} and #{Project.in_used_projects("true", :alias => "projects")}"], 
+              :order => "jobs.updated_at desc", :limit => 5
+              )
+  end
 end
