5cda45bd9604cb9f1e7c7ed38d1076a333c9be3f
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-07-09 08:59:11 +0000

    project: add project_logo_helper

--- of/app/helpers/images_helper.rb
+++ of/app/helpers/images_helper.rb
@@ -1,2 +1,9 @@
 module ImagesHelper
+  def project_logo_link(project, options={})
+  "<a href=/projects/#{project.id}>
+     <img src=\"#{url_for(:controller => :images, :action => "image", 
+                         :id => project.icon, :size => options[:size]||32)}\" 
+         title=\"#{project.name}\" align=#{options[:align]||:middle} />
+    </a>"
+  end
 end
