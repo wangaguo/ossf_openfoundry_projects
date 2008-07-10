f11efdb88dbd7f3dc3b893453d85e2ceeecf3ff1
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-07-10 09:22:11 +0000

    sync: add sync admin action

--- trunk/of/app/controllers/site_admin/site_admin_controller.rb
+++ trunk/of/app/controllers/site_admin/site_admin_controller.rb
@@ -1,15 +1,13 @@
-require 'of'
 class SiteAdmin::SiteAdminController < SiteAdmin
-  include OpenFoundry::Message
   def index
   end
   def resend
     User.find(:all, :conditions => User.verified_users()).each do |u|
-      send_msg(TYPES[:user],ACTIONS[:create],{'id' => u.id, 'name' => u.login})
+      ApplicationController::send_msg(TYPES[:user],ACTIONS[:create],{'id' => u.id, 'name' => u.login})
     end
   
     Project.find(:all, :conditions => Project.in_used_projects()).each do |p|
-      send_msg(TYPES[:project], ACTIONS[:create], {'id' => p.id, 'name' => p.summary, 'summary' => p.description}) 
+      ApplicationController::send_msg(TYPES[:project], ACTIONS[:create], {'id' => p.id, 'name' => p.summary, 'summary' => p.description}) 
     end
     
     redirect_to :action => :index
