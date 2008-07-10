0f266aeb4ff1fb5c2cb1f19548bef5ad4ce8e3f8
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-07-10 09:04:22 +0000

    sync: add sync admin action

--- of/app/controllers/site_admin/site_admin_controller.rb
+++ of/app/controllers/site_admin/site_admin_controller.rb
@@ -1,4 +1,18 @@
+require 'of'
 class SiteAdmin::SiteAdminController < SiteAdmin
   def index
+  end
+  def resend
+    include OpenFoundry::Message
+   
+    User.find(:all, :conditions => User.verified_users()).each do |u|
+      send_msg(TYPES[:user],ACTIONS[:create],{'id' => u.id, 'name' => u.login})
+    end
+  
+    Project.find(:all, :conditions => Project.in_used_projects()).each do |p|
+      send_msg(TYPES[:project], ACTIONS[:create], {'id' => p.id, 'name' => p.summary, 'summary' => p.description}) 
+    end
+    
+    redirect_to :action => :index
   end
 end
