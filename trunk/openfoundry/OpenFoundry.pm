0555f549756b8527bfd29f94a90378970c02f255
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2008-07-11 09:26:32 +0000

    merge classes in OpenFoundry.pm (with kaworu)

--- trunk/openfoundry/OpenFoundry.pm
+++ trunk/openfoundry/OpenFoundry.pm
@@ -7,33 +7,20 @@ use LWP::Simple ();
 use JSON::XS;
 use FileHandle;
 use Fatal qw(open close);
+use Carp;
 
 # see: app/models/project.rb
 # VCS = { :NONE => 0, :CVS => 1, :SUBVERSION => 2, :REMOTE => -1 }.freeze
 use constant { VCS_NONE => 0, VCS_CVS => 1, VCS_SUBVERSION => 2, VCS_REMOTE => -1 };
 
+# Usage: my $of = OpenFoundry->init(); # use the backend specified in the conf
 sub init
 {
-	#my ($class, $backend) = @_;
+	my ($class) = @_;
 	my $conf = loadConf();
-	my $backend = $conf->{DATA_BACKEND};
-	if ($backend eq 'Mock')
-	{
-		return new OpenFoundry::Impl::Mock;
-	}
-	elsif ($backend eq 'OF')
-	{
-		return new OpenFoundry::Impl::OF($conf);
-	}
-	elsif ($backend eq 'RT')
-	{
-		die "The RT backend should no longer be used";
-		#return new OpenFoundry::Impl::RT($conf);
-	}
-	else
-	{
-		die "We have no implementation named '$backend'";
-	}
+	my $self = OpenFoundry::__loadJsonFile($conf->{$> == 0 ? ROOT_JSON_DUMP_CACHE_PATH : JSON_DUMP_CACHE_PATH});
+	$self->{conf} = $conf;
+	return bless $self, $class;
 }
 # static
 # TODO: cache loaded conf ?
@@ -56,93 +43,81 @@ sub __loadJsonFile
 	return decode_json(scalar(<$fh>));
 }
 
-sub getProjects
-{
-	my ($self) = @_;
-	return $self->{'projects'};
-}
-sub getUsers
-{
-	my ($self) = @_;
-	return $self->{'users'};
-}
-sub getRelations
-{
-	my ($self) = @_;
-	return $self->{'relations'};
-}
-
-sub getProjectById
-{
-	my ($self, $id) = @_;
-	foreach my $p (@{$self->getProjects()})
-	{
-		return $p if $p->{'id'} eq $id;
-	}
-	return undef;
-#	return grep { $_->{'id'} eq $id } @{$self->getProjects()};
-}
-sub getProjectByName
-{
-	my ($self, $name) = @_;
-	foreach my $p (@{$self->getProjects()})
-	{
-		return $p if $p->{'name'} eq $name;
-	}
-	return undef;
-}
-sub getUserById
-{
-	my ($self, $id) = @_;
-	foreach my $u (@{$self->getUsers()})
-	{
-		return $u if $u->{'id'} eq $id;
-	}
-	return undef;
-#	return grep { $_->{'id'} eq $id } @{$self->getUsers()};
-}
-sub getUserByName
-{
-	my ($self, $name) = @_;
-	foreach my $u (@{$self->getUsers()})
-	{
-		return $u if $u->{'name'} eq $name;
-	}
-	return undef;
-}
-
-sub isInRelation
-{
-	my ($self, $relation, $projectId, $userId) = @_;
-	foreach (@{$self->getRelations()->{$relation}})
-	{
-		return 1 if $_->[0] eq $projectId and $_->[1] eq $userId;
-	}
-	return 0; 
-	#return grep { $_->[0] eq $projectId and $_->[1] eq $userId } @{$self->getRelations()->{$relation}};
-}
-
-sub isInRelationByName
-{
-	my ($self, $relation, $projectName, $userName) = @_;
-	my $p = $self->getProjectByName($projectName) || return 0;
-	my $u = $self->getUserByName($userName) || return 0;
-	return $self->isInRelation($relation, $p->{id}, $u->{id});
-}
-
-sub getConf
-{
-	my ($self) = @_;
-	return $self->{conf};
-}
-
-
-
-
-sub getSessionInfo
-{
-	die "no default implementation";
-}
+## wrappers
+#sub getProjects
+#{
+#	my ($self) = @_;
+#	return $self->{'projects'};
+#}
+#sub getUsers
+#{
+#	my ($self) = @_;
+#	return $self->{'users'};
+#}
+#sub getFunctions
+#{
+#	my ($self) = @_;
+#	return $self->{'functions'};
+#}
+#
+## private utility, should be faster than grep
+#my $sequential_search = sub {
+#	my ($objs_array_ref, $attr, $val) = @_;
+#	foreach my $obj (@{$objs_array_ref})
+#	{
+#		return $obj if $obj->{$attr} eq $val;
+#	}
+#	return undef;
+#};
+#
+#sub getProjectById
+#{
+#	my ($self, $id) = @_;
+#	return $sequential_search->($self->getProjects(), "id", $id);
+#}
+#sub getProjectByName
+#{
+#	my ($self, $name) = @_;
+#	return $sequential_search->($self->getProjects(), "name", $name);
+#}
+#sub getUserById
+#{
+#	my ($self, $id) = @_;
+#	return $sequential_search->($self->getUsers(), "id", $id);
+#}
+#sub getUserByName
+#{
+#	my ($self, $name) = @_;
+#	return $sequential_search->($self->getUsers(), "name", $name);
+#}
+#
+#sub hasFunction
+#{
+#	my ($self, $function, $projectId, $userId) = @_;
+#	foreach (@{$self->getFunctions()->{$function}})
+#	{
+#		return 1 if $_->[0] eq $projectId and $_->[1] eq $userId;
+#	}
+#	return 0; 
+#}
+#sub hasFunctionByName
+#{
+#	my ($self, $function, $projectName, $userName) = @_;
+#	my $p = $self->getProjectByName($projectName) || return 0;
+#	my $u = $self->getUserByName($userName) || return 0;
+#	return $self->hasFunction($function, $p->{id}, $u->{id});
+#}
+#
+#sub getConf
+#{
+#	my ($self) = @_;
+#	return $self->{conf};
+#}
+#
+#sub getSessionInfo
+#{
+#	die "no default implementation";
+#}
 
 # static
 # TODO: fix me !
@@ -156,15 +131,7 @@ sub _log
 
 ##########################################
 
-package OpenFoundry::Impl::RT;
-use vars qw(@ISA);
-@ISA = ('OpenFoundry');
 
-use LWP::Simple;
-use Data::Dumper;
-use JSON::XS;
-use Carp;
-use Fatal qw(open close);
 
 #vcs# perl -MOpenFoundry -MBenchmark -e 'timethis(100, sub {OpenFoundry->init("RT")})'
 #timethis 100:  3 wallclock secs ( 2.66 usr +  0.08 sys =  2.73 CPU) @ 36.57/s (n=100)
@@ -172,98 +139,33 @@ use Fatal qw(open close);
 #vcs# perl -MOpenFoundry -MBenchmark -e 'timethis(100, sub {OpenFoundry->init("RT")->isInRelationByName("admin", "openfoundry", "LCamel") })'
 #timethis 100:  3 wallclock secs ( 2.75 usr +  0.08 sys =  2.83 CPU) @ 35.36/s (n=100)
 
-sub new
-{
-	my ($class, $conf) = @_;
-	my $self = OpenFoundry::__loadJsonFile($conf->{$> == 0 ? ROOT_JSON_DUMP_CACHE_PATH : JSON_DUMP_CACHE_PATH});
-	$self->{conf} = $conf;
-	return bless $self, $class;
-}
 
 # static (for not loading old data)
-# perl -MOpenFoundry -e 'OpenFoundry::Impl::RT::refresh'
+# perl -MOpenFoundry -e 'OpenFoundry::refresh("vcs")'
 # see: /usr/local/bin/openfoundry_sync_cache.sh
 sub refresh
 {
+	my ($module) = @_;
+	die "no module" if not $module;
 	my $conf = OpenFoundry::loadConf();
-	my $url = $conf->{DUMP_SOURCE_URL} . $conf->{DUMP_SECRET};
+	my $url = sprintf($conf->{DUMP_SOURCE_URL}, $conf->{DUMP_SECRET}, $module);
 	my $json = LWP::Simple::get($url);
 	# TODO: integrity check ??
+	my $obj = decode_json($json);
 	my $umask = umask 0077;
 	open my $fh, ">", $conf->{ROOT_JSON_DUMP_CACHE_PATH};
 	print {$fh} $json;
 	close $fh;
 	umask $umask;
 
-	# write a stripped-down version for non-root users
-	my $obj = decode_json($json);
-	foreach my $user (@{$obj->{users}})
+	my $normal_user_data;
+	if ($module eq "vcs")
 	{
-		$user->{Password} = $user->{Email} = '';
+		$normal_user_data = { "functions" => $obj->{"functions"} };
 	}
-	open my $fh2, ">", $conf->{JSON_DUMP_CACHE_PATH};
-	print {$fh2} encode_json($obj);
-	close $fh2;
-}
-
-
-sub getSessionInfo
-{
-	my ($self, $SID, $projectUnixName) = @_;
-	my $tmp = LWP::Simple::get("http://rt.openfoundry.org/NoAuth/Session.html?SID=$SID&projectUnixName=$projectUnixName");
-	my ($userName, $role, $email) = split / /, $tmp;
-	#_log("userName: $userName role: $role email: $email");
-	return ($userName, $role, $email);
-}
-##########################################
-
-package OpenFoundry::Impl::OF;
-use vars qw(@ISA);
-@ISA = ('OpenFoundry');
-
-use LWP::Simple;
-use Data::Dumper;
-use JSON::XS;
-use Carp;
-use Fatal qw(open close);
-
-#vcs# perl -MOpenFoundry -MBenchmark -e 'timethis(100, sub {OpenFoundry->init("RT")})'
-#timethis 100:  3 wallclock secs ( 2.66 usr +  0.08 sys =  2.73 CPU) @ 36.57/s (n=100)
-#
-#vcs# perl -MOpenFoundry -MBenchmark -e 'timethis(100, sub {OpenFoundry->init("RT")->isInRelationByName("admin", "openfoundry", "LCamel") })'
-#timethis 100:  3 wallclock secs ( 2.75 usr +  0.08 sys =  2.83 CPU) @ 35.36/s (n=100)
-
-sub new
-{
-	my ($class, $conf) = @_;
-	my $self = OpenFoundry::__loadJsonFile($conf->{$> == 0 ? ROOT_JSON_DUMP_CACHE_PATH : JSON_DUMP_CACHE_PATH});
-	$self->{conf} = $conf;
-	return bless $self, $class;
-}
-
-# static (for not loading old data)
-# perl -MOpenFoundry -e 'OpenFoundry::Impl::OF::refresh'
-# see: /usr/local/bin/openfoundry_sync_cache.sh
-sub refresh
-{
-	my $conf = OpenFoundry::loadConf();
-	my $url = $conf->{DUMP_SOURCE_URL} . $conf->{DUMP_SECRET};
-	my $json = LWP::Simple::get($url);
-	# TODO: integrity check ??
-	my $umask = umask 0077;
-	open my $fh, ">", $conf->{ROOT_JSON_DUMP_CACHE_PATH};
-	print {$fh} $json;
-	close $fh;
-	umask $umask;
-
 	# write a stripped-down version for non-root users
-	my $obj = decode_json($json);
-	foreach my $user (@{$obj->{users}})
-	{
-		$user->{password} = $user->{email} = '';
-	}
 	open my $fh2, ">", $conf->{JSON_DUMP_CACHE_PATH};
-	print {$fh2} encode_json($obj);
+	print {$fh2} encode_json($normal_user_data);
 	close $fh2;
 }
 
@@ -279,3 +181,22 @@ sub getSessionInfo
 	#_log("userName: $userName role: $role email: $email");
 	return ($userName, $role, $email);
 }
+
+####################################
+#
+# perl -MOpenFoundry -e 'OpenFoundry::test()'
+#
+sub test
+{
+	my $of = OpenFoundry->init();
+	print Dumper($of);
+	#print "projects: ", join(", ", map { $_->{"name"} } @{$of->getProjects()}), "\n";
+	#my $p = $of->getProjectByName("openfoundry");
+	#print "project openfoundry: ", Dumper($p);
+	#print "users: ", join(", ", map { $_->{"name"} } @{$of->getUsers()}), "\n";
+	#my $u = $of->getUsers()->[0];
+	#print "first user: ", Dumper($u);
+	
+}
+	
+1;
