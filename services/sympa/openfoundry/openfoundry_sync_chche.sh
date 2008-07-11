f9940bd3856035343c1f4b03cf81d7471f39a9af
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-07-11 10:08:52 +0000

    sympa: update sync script

--- services/sympa/openfoundry/openfoundry_sync_chche.sh
+++ services/sympa/openfoundry/openfoundry_sync_chche.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-(date; perl -MOpenFoundry -e 'OpenFoundry::Impl::OF::refresh') >> /var/log/sync_cache_foundry.log 2>&1
+(date; perl -MOpenFoundry -e 'OpenFoundry::refresh("sympa")') >> /var/log/sync_cache_foundry.log 2>&1
