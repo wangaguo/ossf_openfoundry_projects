c5ce7f4e2f12700510e48635f9971218c87e8d1a
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-07-14 07:23:04 +0000

    sync:msg: add function create to 'resend'

--- trunk/of/app/controllers/site_admin/site_admin_controller.rb
+++ trunk/of/app/controllers/site_admin/site_admin_controller.rb
@@ -10,6 +10,19 @@ class SiteAdmin::SiteAdminController < SiteAdmin
       ApplicationController::send_msg(TYPES[:project], ACTIONS[:create], {'id' => p.id, 'name' => p.summary, 'summary' => p.description}) 
     end
     
+    ActiveRecord::Base.connection.select_rows(
+    "select distinct U.id, P.id, F.name from users U, roles_users RU, roles_functions RF, functions F, roles R, projects P where
+             U.id = RU.user_id and 
+             ( ( RU.role_id = RF.role_id and RF.function_id = F.id) or
+             ( R.name = 'Admin' ) ) and
+             RU.role_id = R.id and R.authorizable_type = 'Project' and
+             R.authorizable_id = P.id and 
+             #{User.verified_user(:alias => 'U')} and 
+             #{Project.in_used_projects(:alias => 'P')} order by U.id
+           ").each do |u, p, f|
+             ApplicationController::send_msg(TYPES[:function],ACTIONS[:create],{'user_id' => u, 'project_id' => p, 'function_name' => f})
+    end
+    
     redirect_to :action => :index
   end
 end
