f69eb3078dd27f1ea59c2fa44eea0d7a2af472c8
LCamel <LCamel@users.noreply.github.com>
2008-07-24 06:19:30 +0000

    rt: RT_SiteConfig.pm: adds more local settings

--- services/rt/usr/local/rt3/etc/RT_SiteConfig.pm
+++ services/rt/usr/local/rt3/etc/RT_SiteConfig.pm
@@ -14,11 +14,26 @@
 #
 #   perl -c /path/to/your/etc/RT_SiteConfig.pm
 
-Set( $rtname, 'example.com');
+Set($rtname, 'OSSF');
+Set($Organization, $rtname);
+Set($TicketBaseURI, "fsck.com-rt://$RT::rtname/ticket/");
+Set($WebURL, "http://of.openfoundry.org/rt/");
+
 #Set($LogToFile      , 'debug');
 #Set($LogDir, '/tmp');
 Set($OpenFoundry_SSO, 1);
 Set($OpenFoundry_SSO_Cookie, '_of_session_id');
 Set($OpenFoundry_SSO_URL, 'http://of.openfoundry.org/openfoundry/get_user_by_session_id?session_id='); 
+Set($DatabaseRTHost, '192.168.0.30');
+
+# If you're putting the web ui somewhere other than at the root of
+# your server, you should set $WebPath to the path you'll be
+# serving RT at.
+# $WebPath requires a leading / but no trailing /.
+#
+# In most cases, you should leave $WebPath set to '' (an empty value).
+
+Set($WebPath , "/rt");
+#Set($WebBaseURL, "http://rt.of.openfoundry.org");
 
 1;
