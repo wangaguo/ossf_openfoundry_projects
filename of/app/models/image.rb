e5e265332c142beada2b3c8fba998639bc23ed6c
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-07-25 12:32:29 +0000

    image: change image store from db to file store

--- of/app/models/image.rb
+++ of/app/models/image.rb
@@ -1,10 +1,14 @@
-require 'RMagick'
-
 class Image < ActiveRecord::Base
   IMAGE_UNKNOWN_ID = 65535
-  
+  IMAGE_CACHES_DIR = "#{RAILS_ROOT}/tmp/image_caches"
+  IMAGE_DATA_DIR = "#{RAILS_ROOT}/tmp/image_data"
+
   validates_format_of :meta, :with => /^image/,
-    :message => "picture only"
+    :message => _("Image|Picture only")
+
+  def save_to_file
+    File.open("#{IMAGE_DATA_DIR}/#{self.id}","w+").write(self.data)
+  end
 
   def image=(picture_field)
     self.name = base_part_of(picture_field.original_filename)
@@ -12,8 +16,9 @@ class Image < ActiveRecord::Base
     self.data = picture_field.read
     
     #縮圖!
-    img = Magick::Image.from_blob(self.data).first
-    self.data = img.resize!(128,128).to_blob
+    #img = Magick::Image.from_blob(self.data).first
+    #self.data = img.resize!(128,128).to_blob
+
   end
   
   def base_part_of(file_name)
