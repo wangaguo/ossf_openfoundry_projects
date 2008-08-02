5eff8f223cbf573f26679a06aea01633eb450d16
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-08-02 05:25:48 +0000

    monitor: add index server

--- trunk/of/misc/openfoundry.god.rb
+++ trunk/of/misc/openfoundry.god.rb
@@ -7,26 +7,34 @@ RUBY = "/home/openfoundry/ruby/ruby/bin/ruby"
 MONGREL_RAILS = "/home/openfoundry/ruby/ruby/bin/mongrel_rails"
 RAILS_ROOT = "/home/openfoundry/of"
 ENVIRONMENT = "production"
+#ENVIRONMENT = "development"
 
 USER = 'openfoundry'
 GROUP = 'openfoundry'
 
 ADDRESS = '127.0.0.1'
 
-#for internal use, like foundry_sync, sso
+#7998,7999 for internal use, like foundry_sync, sso
+#8000-8004 for production 
+#9000      for ferret index server
 7998.upto(8004) do |port|
   God.watch do |w|
+    if port > 7999
+      w.group = "openfoundry"
+    else
+      w.group = "openfoundry-sync"
+    end
+
     w.name = "openfoundry-mongrel-#{port}"
     w.interval = 30.seconds # default      
     w.start = "#{RUBY} #{MONGREL_RAILS} start -e #{ENVIRONMENT} -c #{RAILS_ROOT} -p #{port} \
       --user #{USER} --group #{GROUP} -l #{RAILS_ROOT}/log/mongrel.#{port}.log \
       -P #{RAILS_ROOT}/tmp/pids/mongrel.#{port}.pid  -d"
     w.stop = "#{RUBY} #{MONGREL_RAILS} stop -P #{RAILS_ROOT}/tmp/pids/mongrel.#{port}.pid"
-    w.restart = "#{RUBY} #{MONGREL_RAILS} restart -e #{ENVIRONMENT} -c #{RAILS_ROOT} -p #{port} \
-      --user #{USER} --group #{GROUP} -l #{RAILS_ROOT}/log/mongrel.#{port}.log \
-      -P #{RAILS_ROOT}/tmp/pids/mongrel.#{port}.pid  -d"
-    w.start_grace = 10.seconds
-    w.restart_grace = 10.seconds
+    w.restart = "#{RUBY} #{MONGREL_RAILS} restart -c #{RAILS_ROOT} \
+      -P #{RAILS_ROOT}/tmp/pids/mongrel.#{port}.pid" 
+    w.start_grace = 5.seconds
+    w.restart_grace = 5.seconds
     w.pid_file = File.join(RAILS_ROOT, "tmp/pids/mongrel.#{port}.pid")
     
     w.behavior(:clean_pid_file)
@@ -71,7 +79,7 @@ ADDRESS = '127.0.0.1'
       
       on.condition(:cpu_usage) do |c|
         c.interval = 10
-        c.above = 70.percent
+        c.above = 90.percent
         c.times = [3, 5]
         c.notify = 'ossf-dev'
       end
@@ -90,6 +98,83 @@ ADDRESS = '127.0.0.1'
 
         c.notify = 'tim'
       end
+    end
+  end
+end
+
+God.watch do |w|
+  command = "su #{USER} -c 'cd #{RAILS_ROOT};#{RUBY} #{RAILS_ROOT}/script/ferret_server -e #{ENVIRONMENT} %s'"
+  w.group = "openfoundry-index-server"
+
+  w.name = "openfoundry-ferret-server-9000"
+  w.interval = 30.seconds # default      
+  w.start = command % 'start'
+  w.stop = command % 'stop'
+  w.restart = command % "stop;#{RUBY} #{RAILS_ROOT}script/ferret_server -e #{ENVIRONMENT} start"
+  w.start_grace = 20.seconds
+  w.restart_grace = 20.seconds
+  w.pid_file = File.join(RAILS_ROOT, "log/ferret.pid")
+  
+  w.behavior(:clean_pid_file)
+  
+  # determine the state on startup
+  w.transition(:init, { true => :up, false => :start }) do |on|
+    on.condition(:process_running) do |c|
+      c.running = true
+    end
+  end
+  
+  # determine when process has finished starting
+  w.transition([:start, :restart], :up) do |on|
+    on.condition(:process_running) do |c|
+      c.running = true
+      c.notify = 'tim'
+    end
+    
+    # failsafe
+    on.condition(:tries) do |c|
+      c.times = 5
+      c.transition = :start
+      c.notify = 'tim'
+    end
+  end
+
+  # start if process is not running
+  w.transition(:up, :start) do |on|
+    on.condition(:process_exits) do |c|
+      c.notify = 'ossf-dev'
+    end
+  end
+  
+  # restart if memory or cpu is too high
+  w.transition(:up, :restart) do |on|
+    on.condition(:memory_usage) do |c|
+      c.interval = 20
+      c.above = 50.megabytes
+      c.times = [3, 5]
+      c.notify = 'ossf-dev'
+    end
+    
+    on.condition(:cpu_usage) do |c|
+      c.interval = 10
+      c.above = 70.percent
+      c.times = [3, 5]
+      c.notify = 'ossf-dev'
+    end
+  end
+  
+  # lifecycle
+  w.lifecycle do |on|
+    on.condition(:flapping) do |c|
+      c.to_state = [:start, :restart]
+      c.times = 5
+      c.within = 5.minute
+      c.transition = :unmonitored
+      c.retry_in = 10.minutes
+      c.retry_times = 5
+      c.retry_within = 2.hours
+
+      c.notify = 'tim'
     end
   end
 end
