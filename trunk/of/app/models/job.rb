0a5775efb9f7d0ff62ad16dc55e083cfb398db4a
wangaguo <wangaguo@eab794a4-67ee-0310-8774-9eb661efe622>
2008-08-03 02:52:49 +0000

    Job: add ferret search

--- trunk/of/app/models/job.rb
+++ trunk/of/app/models/job.rb
@@ -2,6 +2,33 @@ class Job < ActiveRecord::Base
   belongs_to :project, :foreign_key => "project_id"
   STATUS = {:Enabled => 1, :Disabled => 0}
 
+  #add fulltext indexed SEARCH
+  acts_as_ferret({
+                  :fields => { 
+                              :subject => { :boost => 1.5,
+                                          :store => :yes,
+                                          :index => :yes },
+                              :description => { :store => :yes,
+                                             :index => :yes },  
+                              :requirement => { :store => :yes,
+                                             :index => :yes }
+                            },
+                  :single_index => true,
+                  :default_field => [:subject, :description, :requirement]
+                 },{ :analyzer => GENERIC_ANALYZER })
+  
+  def should_be_indexed?
+    self.status == Job::STATUS[:Enabled]
+  end
+  def ferret_enabled?(is_bulk_index = false)
+    should_be_indexed? && #super(is_bulk_index) # TODO: super will cause recursive call..
+      (@ferret_disabled.nil? && (is_bulk_index || self.class.ferret_enabled?))
+  end
+  def destroy_ferret_index_when_not_ready
+    ferret_destroy if not should_be_indexed?
+  end
+  after_save :destroy_ferret_index_when_not_ready
+  
   validates_length_of :subject, :within => 3..255, :too_long => _("Length range is ") + "3-255", :too_short => _("Length range is ") + "3-255"
   validates_length_of :description, :within => 3..4000, :too_long => _("Length range is ") + "3-4000", :too_short => _("Length range is ") + "3-4000"
   validates_inclusion_of :status, :in => STATUS.values, :message => _("Not a valid value")
