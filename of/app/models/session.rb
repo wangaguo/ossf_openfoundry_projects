7a16b855fdcc468126922d25805422d0a557be80
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-08-19 09:19:00 +0000

    session: change online user count 25 -> 20

--- of/app/models/session.rb
+++ of/app/models/session.rb
@@ -1,7 +1,7 @@
 class Session < ActiveRecord::Base
   class << self
     def online_users
-      Session.find_by_sql("select distinct(user_id) from sessions where user_id is not null and updated_at > '#{OPENFOUNDRY_SESSION_EXPIRES_AFTER.ago.strftime '%Y-%m-%d %H:%M:%S'}' order by updated_at desc limit 25").collect{|s| User.find_by_id(s.user_id)}.reject{|u| u.t_conceal_login}
+      Session.find_by_sql("select distinct(user_id) from sessions where user_id is not null and updated_at > '#{OPENFOUNDRY_SESSION_EXPIRES_AFTER.ago.strftime '%Y-%m-%d %H:%M:%S'}' order by updated_at desc limit 20").collect{|s| User.find_by_id(s.user_id)}.reject{|u| u.t_conceal_login}
     end
 
   end
