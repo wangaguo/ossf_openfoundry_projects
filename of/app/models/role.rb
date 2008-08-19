4dbb7fb6c8498c7c5d073d3810bc3f8483465416
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-08-19 09:19:53 +0000

    role: add methods for default roles

--- of/app/models/role.rb
+++ of/app/models/role.rb
@@ -7,6 +7,15 @@ class Role < ActiveRecord::Base
   has_and_belongs_to_many :functions, :join_table => 'roles_functions'
   belongs_to :authorizable, :polymorphic => true
 
+  def editable?
+    self.name.downcase != 'admin'
+  end
+  
+  def deletable?
+    self.name.downcase != 'admin' and
+      self.name.downcase != 'member'
+  end
+  
   def self.set_default_privileges_for(role)
     case role.name.downcase
     when 'admin'
