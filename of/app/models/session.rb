bf6da903627d021bb2511c4b9300081d6ea9e8a7
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-08-23 04:12:23 +0000

    session: update online user selection

--- of/app/models/session.rb
+++ of/app/models/session.rb
@@ -1,7 +1,20 @@
 class Session < ActiveRecord::Base
   class << self
-    def online_users
-      Session.find_by_sql("select distinct(user_id) from sessions where user_id is not null and updated_at > '#{OPENFOUNDRY_SESSION_EXPIRES_AFTER.ago.strftime '%Y-%m-%d %H:%M:%S'}' order by updated_at desc limit 20").collect{|s| User.find_by_id(s.user_id)}.reject{|u| u.t_conceal_login}
+    def online_users(options = {})
+     limit = options[:limit] || 20
+     count = 0
+     rtn = []
+     while(rtn.length < limit)
+       u = User.find_by_sql("select U.* from sessions S left join users U on S.user_id = U.id 
+           where S.user_id is not null and
+           S.updated_at > '#{OPENFOUNDRY_SESSION_EXPIRES_AFTER.ago.strftime '%Y-%m-%d %H:%M:%S'}'
+                  order by S.updated_at desc 
+                            limit 1 offset #{count}").first
+       break unless u                     
+       rtn << u unless u.t_conceal_login                     
+       count=count+1
+     end
+     rtn
     end
 
   end
