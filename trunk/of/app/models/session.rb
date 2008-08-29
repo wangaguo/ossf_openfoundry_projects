63ed443be5d292eca19a1e868bef216c6eefd8d2
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-08-29 07:04:33 +0000

    session: fix timezone, user duplicated and expire-after bug

--- trunk/of/app/models/session.rb
+++ trunk/of/app/models/session.rb
@@ -7,11 +7,11 @@ class Session < ActiveRecord::Base
      while(rtn.length < limit)
        u = User.find_by_sql("select U.* from sessions S left join users U on S.user_id = U.id 
            where S.user_id is not null and
-           S.updated_at > '#{OPENFOUNDRY_SESSION_EXPIRES_AFTER.ago.strftime '%Y-%m-%d %H:%M:%S'}'
+           S.updated_at > subdate( now(), interval #{OPENFOUNDRY_SESSION_EXPIRES_AFTER} second)
                   order by S.updated_at desc 
                             limit 1 offset #{count}").first
        break unless u                     
-       rtn << u unless u.t_conceal_login                     
+       rtn << u unless( u.t_conceal_login or rtn.member? u )                    
        count=count+1
      end
      rtn
