5783ac0fbb61ff8032b22018c2fa3d6ce487c438
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2008-09-07 12:09:02 +0000

    user: modify current_user for switch as users (su)

--- trunk/of/app/controllers/application.rb
+++ trunk/of/app/controllers/application.rb
@@ -92,7 +92,9 @@ class ApplicationController < ActionController::Base
   
   #before_filter :login_required
   def current_user(session = session())
-    session['user'] || User.find_by_login('guest') # TODO: fix it !!!!!
+    session['effective_user'] || session['user'] || User.find_by_login('guest') 
+    #effective_user for site_admin 'su' to others 
+    #user is your 'normal login user'
   end
   def login?
     current_user().login != 'guest' 
