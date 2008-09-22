cf7ce02152e144bd5fd03a28740cf1bc56f9496c
LCamel <LCamel@eab794a4-67ee-0310-8774-9eb661efe622>
2008-09-22 10:00:48 +0000

    project: more messages on rejecting projects

--- trunk/of/app/models/project_notify.rb
+++ trunk/of/app/models/project_notify.rb
@@ -34,9 +34,7 @@ class ProjectNotify < ActionMailer::Base
   def rejected(project, sent_at = Time.now)
     @subject    = "Your project creation request '#{project.name}' has been rejected!"
     # TODO: maybe only the creator can see the rejected project
-    @body       = { :project_url => url_for(:controller => 'projects',
-                                            :action => 'show',
-                                            :id => project.id) }
+    @body       = { :project_name => project.name }
     @recipients = User.find(project.creator).email
     @from       = OPENFOUNDRY_SITE_ADMIN_EMAIL
     @sent_on    = sent_at
