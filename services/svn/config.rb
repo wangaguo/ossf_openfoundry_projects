15e459b1d514ff37cd0f72595e69064d9f7f56a1
LCamel <LCamel@users.noreply.github.com>
2008-10-23 10:21:49 +0000

    svn: templatize apache_svn.conf

--- services/svn/config.rb
+++ services/svn/config.rb
@@ -8,6 +8,23 @@ def append_unless(path, pattern, &blk)
   File.open(path, "a", &blk) unless found
 end
 
+def open_eval(path)
+  s = File.open(path).read
+  s = "<<\"KKKKKK\"\n#{s}\nKKKKKK"
+  eval(s).chomp
+end
+
+def replace_template(tmpl_path)
+  if tmpl_path.match(/(.*)\.tmpl$/)
+    s = open_eval(tmpl_path)
+    File.open($1, "w") { |f| f.write s }
+  else
+    puts "Please ends with .tmpl: #{tmpl_path}"
+  end
+end
+
+
+
 load File.dirname(__FILE__) + "/svn.conf"
 
 RUBY = Config::CONFIG["bindir"] + "/" + Config::CONFIG["RUBY_INSTALL_NAME"]
@@ -31,3 +48,5 @@ append_unless("/etc/newsyslog.conf", /openfoundry_sync.log/) do |f|
 {ROOT}/backup.log         600  7     *    @T00  JC
 "
 end
+
+replace_template("apache_svn.conf.tmpl")
