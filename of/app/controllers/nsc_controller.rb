a4453788250667eb47126a548200495087878c31
LCamel <LCamel@users.noreply.github.com>
2008-12-03 09:36:23 +0000

    nsc: report upload status

--- of/app/controllers/nsc_controller.rb
+++ of/app/controllers/nsc_controller.rb
@@ -4,4 +4,13 @@ class NscController < ApplicationController
   def download_count
 
   end
+  def report_status
+    @project_name_to_files = {}
+    Dir.glob("#{NSC_UPLOAD_DIR}/*") do |path|
+      basename = File.basename(path)
+      if basename.match(/(.*?)_/)
+        (@project_name_to_files[$1] ||= []) << basename
+      end
+    end
+  end
 end
