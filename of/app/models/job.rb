fcf6313782800f5f999d8123a4c850d060efb3d9
wangaguo <wangaguo@gmail.com>
2009-02-05 08:25:50 +0000

    Job: Date validation. use "validates_timeliness" plugin.

--- of/app/models/job.rb
+++ of/app/models/job.rb
@@ -32,12 +32,7 @@ class Job < ActiveRecord::Base
   validates_length_of :subject, :within => 3..255, :too_long => _("Length range is ") + "3-255", :too_short => _("Length range is ") + "3-255"
   validates_length_of :description, :within => 3..4000, :too_long => _("Length range is ") + "3-4000", :too_short => _("Length range is ") + "3-4000"
   validates_inclusion_of :status, :in => STATUS.values, :message => _("Not a valid value")
-  
-  def validate
-    date_pattern = /^(19|20)\d\d[- \/.](0?[1-9]|1[012])[- \/.](0?[1-9]|[12][0-9]|3[01])$/
-    errors.add(:due, "must be formatted YYYY-MM-DD or YYYY/MM/DD") if
-    due.nil? || date_pattern !~ due_before_type_cast
-  end
+  validates_date :due
   
   def self.AllProjectLatestJobs
     Job.find(:all, :joins => :project, 
