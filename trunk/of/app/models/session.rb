f5f55f3575c654fa8c86e19954bc021bc4fb94e3
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2009-02-11 06:06:10 +0000

    session:memcache: bug fix, use no instance var

--- trunk/of/app/models/session.rb
+++ trunk/of/app/models/session.rb
@@ -1,27 +1,25 @@
 require 'memcache'
 class Session < ActiveRecord::Base
   class << self
-    # set ::Rails.cache shortcut
-    @cache = ::Rails.cache
     def user_login(user_id)
       access_memcache_with_lock do 
-        online = @cache.read('session-pool') || {}
+        online = ::Rails.cache.read('session-pool') || {}
         online[user_id] = Time.now
-        @cache.write('session-pool', online)
+        ::Rails.cache.write('session-pool', online)
       end
     end
 
     def user_logout(user_id)
       access_memcache_with_lock do 
-        online = @cache.read('session-pool') || {}
-        online.delete!(user_id)
-	@cache.write('session-pool', online)
+        online = ::Rails.cache.read('session-pool') || {}
+        online.delete(user_id)
+	::Rails.cache.write('session-pool', online)
       end
     end
 
     def online_users(options = {})
      limit = options[:limit] || 20
-     online = @cache.read('session-pool')
+     online = ::Rails.cache.read('session-pool')||{}
      user_ids = online.keys.compact
      user_ids.sort!{|u1,u2| online[u1] <=> online[u2] }
      rtn = []
@@ -38,10 +36,10 @@ class Session < ActiveRecord::Base
 
     def access_memcache_with_lock
       while true
-        next if @cache.exist?('session-pool:lock')
-        @cache.write('session-pool:lock','!')
+        next if ::Rails.cache.exist?('session-pool:lock')
+        ::Rails.cache.write('session-pool:lock','!')
 	yield
-        @cache.delete('session-pool:lock')
+        ::Rails.cache.delete('session-pool:lock')
         break
       end
     end
