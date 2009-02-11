46deeedf1c9674a0f63a038edcc842e031945370
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2009-02-11 04:14:55 +0000

    sesison:memcache: change session store to memcache, and add lock

--- of/app/models/session.rb
+++ of/app/models/session.rb
@@ -1,21 +1,49 @@
+require 'memcache'
 class Session < ActiveRecord::Base
   class << self
+    # set ::Rails.cache shortcut
+    @cache = ::Rails.cache
+    def user_login(user_id)
+      access_memcache_with_lock do 
+        online = @cache.read('session-pool') || {}
+        online[user_id] = Time.now
+        @cache.write('session-pool', online)
+      end
+    end
+
+    def user_logout(user_id)
+      access_memcache_with_lock do 
+        online = @cache.read('session-pool') || {}
+        online.delete!(user_id)
+	@cache.write('session-pool', online)
+      end
+    end
+
     def online_users(options = {})
      limit = options[:limit] || 20
-     count = 0
+     online = @cache.read('session-pool')
+     user_ids = online.keys.compact
+     user_ids.sort!{|u1,u2| online[u1] <=> online[u2] }
      rtn = []
      while(rtn.length < limit)
-       u = User.find_by_sql("select U.* from sessions S left join users U on S.user_id = U.id 
-           where S.user_id is not null and
-           S.updated_at > subdate( now(), interval #{OPENFOUNDRY_SESSION_EXPIRES_AFTER} second)
-                  order by S.updated_at desc 
-                            limit 1 offset #{count}").first
-       break unless u                     
+       u = User.valid_users.find_by_id(user_ids.pop)	     
+       break unless u 
        rtn << u unless( u.t_conceal_login or rtn.member? u )                    
-       count=count+1
+       break if user_ids.empty?
      end
      rtn
     end
 
+    private 
+
+    def access_memcache_with_lock
+      while true
+        next if @cache.exist?('session-pool:lock')
+        @cache.write('session-pool:lock','!')
+	yield
+        @cache.delete('session-pool:lock')
+        break
+      end
+    end
   end
 end
