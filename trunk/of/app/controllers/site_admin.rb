b75d43affdc83adb438ab964ca814e38a977b6c1
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2009-03-27 04:07:16 +0000

    site_admin: add login redirect, project_review role

--- trunk/of/app/controllers/site_admin.rb
+++ trunk/of/app/controllers/site_admin.rb
@@ -1,8 +1,10 @@
 class SiteAdmin < ApplicationController 
   layout "admin"
+  before_filter :login_required
   before_filter :check_site_admin
 
   def check_site_admin
-    redirect_to :controller => '../openfoundry' if not current_user().has_role?('site_admin')
+    redirect_to :controller => '../openfoundry' unless( current_user().has_role?('site_admin') or 
+                                                      current_user.has_role>('project_reviewer') )
   end
 end
