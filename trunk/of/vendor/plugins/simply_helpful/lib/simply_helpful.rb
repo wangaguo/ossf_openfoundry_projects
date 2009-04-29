b65f70c0c264405a33ea7f152e80d4bfc591b4c4
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2009-04-29 01:45:38 +0000

    plugin:ror232: remove extensions, not compatible

--- trunk/of/vendor/plugins/simply_helpful/lib/simply_helpful.rb
+++ trunk/of/vendor/plugins/simply_helpful/lib/simply_helpful.rb
@@ -4,7 +4,7 @@ require 'simply_helpful/record_tag_helper'
 
 require 'simply_helpful/polymorphic_routes'
 
-require 'simply_helpful/jsg_extensions'
-require 'simply_helpful/av_extensions'
-require 'simply_helpful/form_helper_extensions'
-require 'simply_helpful/controller_extensions'
+#require 'simply_helpful/jsg_extensions'
+#require 'simply_helpful/av_extensions'
+#require 'simply_helpful/form_helper_extensions'
+#require 'simply_helpful/controller_extensions'
