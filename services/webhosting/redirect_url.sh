4e8ab094dfc15b64edbf219de7eb63fd779bea91
Shawn C.H. Chiou <shawn-chiou@users.noreply.github.com>
2009-09-09 08:14:03 +0000

    A testing script that won't work properly

--- services/webhosting/redirect_url.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-PATH=${PATH}:/usr/local/bin
-mysql -uroot -B -s -e "SELECT name,redirecturl FROM of_development.projects WHERE redirecturl IS NOT NULL" > /usr/local/webhosting/conf/redirect.txt.new
-mv /usr/local/webhosting/conf/redirect.txt.new /usr/local/webhosting/conf/redirect.txt
