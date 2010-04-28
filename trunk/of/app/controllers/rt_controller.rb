8ad6abe7b7d225c313ecf3246dfbe9c932712334
wangaguo <wangaguo@eab794a4-67ee-0310-8774-9eb661efe622>
2010-04-28 09:41:56 +0000

    bugfix for r2087: e.g. /rt/owner will no permission.

--- trunk/of/app/controllers/rt_controller.rb
+++ trunk/of/app/controllers/rt_controller.rb
@@ -2,7 +2,9 @@ class RtController < ApplicationController
   layout 'rt'
   before_filter :get_project
   def get_project
-    @project = get_project_by_id_or_name(params[:project_id]) { |id| redirect_to :project_id => id }
+    if(params[:project_id] != nil)
+      @project = get_project_by_id_or_name(params[:project_id]) { |id| redirect_to :project_id => id }
+    end
   end
 
   def index
