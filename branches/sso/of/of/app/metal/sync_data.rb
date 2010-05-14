87edca7afc5f63f2cd5a1590f0f4ff015ae248ef
river <river@eab794a4-67ee-0310-8774-9eb661efe622>
2010-05-14 02:49:03 +0000

    Modify the communication protocols of sync accounts between SSO and OF.

--- branches/sso/of/of/app/metal/sync_data.rb
+++ branches/sso/of/of/app/metal/sync_data.rb
@@ -12,38 +12,45 @@ class SyncData
 
   def self.call(env)
 
-    if env["PATH_INFO"] =~ /^\/sync_data/
+    if env["PATH_INFO"] =~ /^\/of\/sync_data/
 
 			if req = Rack::Request.new(env) and req.post? and req.params["data"]
-				userdata = JSON.parse(req.params["data"])
+				userdata = JSON.parse(req.params["data"])["user"]
+				userdata.delete("id")
 
 				case req.params["action"]
-				when "add"
-					syncuser = User.new
-					usertags = userdata.select{ |k, v| k=~ /^t_/ }
+				when "create"
+					if User.find(:first, :conditions => {:login => userdata['name']})
+						[200, {"Content-Type" => "text/html"}, ["true"]]
+					else
+						syncuser = User.new(:login => userdata['name'])
+						usertags = userdata.select{ |k, v| k=~ /^t_/ } || {}
 					
-					userdata.each { |k, v| syncuser.send(k + "=", v) }
-					(usertags.each { |k, v| syncuser.send(k + "=", v) } if syncuser.save)? 
-						[200, {"Content-Type" => "text/html"}, ["ADD_OK"]] :
-						[200, {"Content-Type" => "text/html"}, ["ADD_ERROR"]]
-				when "alter"
-					if syncuser	= User.find(:first, :conditions => {:login => userdata["login"]})
-						userdata.delete("login")
+						userdata.each { |k, v| syncuser.send(k + "=", v) if syncuser.respond_to? "#{k}=" }
+						(usertags.each { |k, v| syncuser.send(k + "=", v) } if syncuser.save)? 
+							[200, {"Content-Type" => "text/html"}, ["true"]] :
+							[200, {"Content-Type" => "text/html"}, ["false"]]
+					end
+				when "update"
+					if syncuser	= User.find(:first, :conditions => {:login => userdata['name']})
+						userdata.delete('name')
 
-						(userdata.each { |k, v| syncuser.send(k + "=", v) }; syncuser.save if not userdata.empty?)?
-							[200, {"Content-Type" => "text/html"}, ["ALTER_OK"]] :
-							[200, {"Content-Type" => "text/html"}, ["ALTER_ERROR"]]
+						(userdata.each { |k, v| syncuser.send(k + "=", v) if syncuser.respond_to? "#{k}=" }; syncuser.save if not userdata.empty?)?
+							[200, {"Content-Type" => "text/html"}, ["true"]] :
+							[200, {"Content-Type" => "text/html"}, ["false #{syncuser.errors}"]]
+					else
+						[200, {"Content-Type" => "text/html"}, ["false, not found: #{userdata["name"]}"]]
 					end
 				else
-					[200, {"Content-Type" => "text/html"}, ["NO_ACTION"]]
+					[200, {"Content-Type" => "text/html"}, ["false, action error"]]
 				end
 
 			else
-				[200, {"Content-Type" => "text/html"}, ["NO_DATA"]]
+				[200, {"Content-Type" => "text/html"}, ["false"]]
     	end
 
 		else
-			[404, {"Content-Type" => "text/html"}, ["Not Found"]]
+			[404, {"Content-Type" => "text/html"}, ["false"]]
   	end
 
 	end
