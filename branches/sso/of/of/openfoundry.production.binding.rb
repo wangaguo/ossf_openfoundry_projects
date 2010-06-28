7d7be126aab45ca86fda13b1479249c9bc7784db
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2010-06-28 03:05:33 +0000

    add variables

--- branches/sso/of/of/openfoundry.production.binding.rb
+++ branches/sso/of/of/openfoundry.production.binding.rb
@@ -3,8 +3,9 @@
 #------------------# 
 #  Generel config  #
 #------------------#
-ip_prefix = '192.168.6'
-host = 'dev.openfoundry.org'
+ip_prefix = '140.109'
+host = 'ssodev.openfoundry.org'
+site_admin_mail = 'hyder.ossf@gmail.com'
 
 #-------------------#
 #  Session config
@@ -15,16 +16,16 @@ session_domain = ".#{host}"
 session_store = ':mem_cache_store'
 
 session_memcache_namespace = 'of-#{RAILS_ENV}'
-session_memcache_server = "#{ip_prefix}.1:11211"
+session_memcache_server = "#{ip_prefix}.22.15:11211"
 
 #-------------------#
 #  Database config  #
 #-------------------#
-db_host = "#{ip_prefix}.10"
-db_name_dev  = 'ofdev'
+db_host = "#{ip_prefix}.22.140"
+db_name_dev  = 'of_development'
 db_name_test = 'of_test'
-db_name_prod = 'ofdev'
-db_user = 'ossf'
+db_name_prod = 'of_development'
+db_user = 'openfoundry'
 
 #----------------------------#
 #  Rails environment config  #
@@ -35,7 +36,7 @@ rails_cache = ':memcache'
 #  Stomp server config
 #  in broker.yml
 #----------------------#
-stomp_host = "#{ip_prefix}.1"
+stomp_host = "#{ip_prefix}.22.140"
 stomp_user = 'openfoundry'
 
 
@@ -43,7 +44,29 @@ stomp_user = 'openfoundry'
 #  Ferret server config
 #  in ferret_server.yml
 #------------------------#
-ferret_host = "#{ip_prefix}.20"
+ferret_host = "#{ip_prefix}.22.15"
 
 
+#------------------------#
+#  Environment config
+#  in environment.rb
+#------------------------#
 
+cache_server = "127.0.0.1:11211"
+cache_server_namespace = 'of-#{RAILS_ENV}'
+
+
+#------------------------#
+#  Redis server config
+#  in environment.rb
+#------------------------#
+
+redis_server = "127.0.0.1"
+
+#------------------------#
+#  sso config
+#  in sso.rb
+#------------------------#
+
+sso_ui_host = '140.109.22.20'
+sso_of_auth_key = 'FIX ME'
