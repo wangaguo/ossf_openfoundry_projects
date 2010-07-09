78ad8da915214b7f8eafb989c43cb061ea3dc722
shawn_chiou <shawn_chiou@eab794a4-67ee-0310-8774-9eb661efe622>
2010-07-09 05:11:48 +0000

    Add for MQ client sync that sync in cron

---
+++ trunk/services/rt/sync_cron.sh
@@ -0,0 +1,23 @@
+OSSF_CHECKOUT=`pwd`
+RUBY=`which ruby`
+
+SYNC_FILE="${OSSF_CHECKOUT}/sync.rb"
+PID_FILE="${OSSF_CHECKOUT}/sync_cron.pid"
+LOG_FILE="${OSSF_CHECKOUT}/rt_sync.log"
+
+if [ -f $PID_FILE ]; then
+        pid=`cat $PID_FILE`
+        if [ -z "${pid}" ]; then
+                rm $PID_FILE
+        else
+                ans=`ps aux ${pid} | grep ${pid}`
+                if [ -z "${ans}" ]; then
+                        rm $PID_FILE
+                fi
+        fi
+fi
+if [ ! -f $PID_FILE ]; then
+        #. $RUBY_SOURCE
+        $RUBY -I $OSSF_CHECKOUT $SYNC_FILE >> $LOG_FILE 2>&1 &
+        echo $! > $PID_FILE
+fi
