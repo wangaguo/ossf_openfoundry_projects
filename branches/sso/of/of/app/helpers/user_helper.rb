2c8fbb728522d8e79569bbc3abd838975a66cebc
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2010-07-09 05:40:58 +0000

    change user image display size; remove unused constants

--- branches/sso/of/of/app/helpers/user_helper.rb
+++ branches/sso/of/of/app/helpers/user_helper.rb
@@ -24,26 +24,15 @@ module UserHelper
     rdf_tag = options[:rdf_tag] || ''
     "<div class=\"user_portrait\" title=\"#{user.login}\" 
     style=\"#{options[:float] ? "float:#{options[:float]};" : 'display:inline;'} 
-    height:#{options[:with_login] ? '55' : '32' }px; 
-    width:#{options[:with_login] ? '60' : '32' }px; 
+    height:#{options[:with_login] ? '70' : '60' }px; 
+    width:#{options[:with_login] ? '60' : '60' }px; 
     border:dotted 1px #eee; text-align:center; vertical-align:text-bottom; line-height: normal;  
     white-space:normal; word-break:break-all; overflow:hidden;\">
     #{options[:link]? "<a #{rdf_tag} href=\"#{options[:link]}\">" : '' }
      <img src=\"http://#{SSO_HOST}/sso/user/image?name=#{user.login}&amp;size=medium\"
-      align=\"#{options[:align]||:middle}\" />
+      align=\"#{options[:align]||:middle}\" width=32/>
      #{options[:with_login] ? "<br/> #{user.login}" : '' }
     #{options[:link]? "</a>":'' }
     </div>"
-  end
-
-  SSO_URL = 'http://ssodev.openfoundry.org/sso/user/'
-  def sso_login_url
-    SSO_URL+'login'
-  end
-  def sso_logout_url
-    SSO_URL+'logout'
-  end
-  def sso_signup_url
-    SSO_URL+'signup'
   end
 end
