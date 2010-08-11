7d3d6da4f1cacecff7fe27500912dbb5a3519952
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2010-08-11 03:18:33 +0000

    fix path problem

--- branches/sso/of/of/vendor/gems/acts_as_ferret-0.4.3/lib/server_manager.rb
+++ branches/sso/of/of/vendor/gems/acts_as_ferret-0.4.3/lib/server_manager.rb
@@ -35,8 +35,7 @@ end
 begin
   ENV['FERRET_USE_LOCAL_INDEX'] = 'true'
   ENV['RAILS_ENV'] = $ferret_server_options['environment']
-  #require(File.join(File.dirname(__FILE__), '../../../../config/environment'))
-  require(File.join(ENV['PWD'], 'config/environment'))
+  require(File.join(File.dirname(__FILE__), '../../../../config/environment'))
   require 'acts_as_ferret'
   ActsAsFerret::Remote::Server.new.send($ferret_server_action)
 rescue Exception => e
