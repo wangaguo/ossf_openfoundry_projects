d5435e4dde1e86a656a4b37a24ae86cfe58173c5
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2010-08-11 03:19:36 +0000

    bug fix: stomp msg sync

--- branches/sso/of/of/lib/of.rb
+++ branches/sso/of/of/lib/of.rb
@@ -17,19 +17,19 @@ module OpenFoundry
 
     # for deliver ACTIONS, like CRUD
     ACTIONS = {
-      :create => 'create', 
-      :update => 'update', 
-      :delete => 'delete'}.freeze
+      :create => :create, 
+      :update => :update, 
+      :delete => :delete}.freeze
     
     # for delivery TYPE, like object type
     TYPES = {
-      :project => 'project', 
-      :user => 'user' , 
-      :function => 'function'}.freeze
+      :project => :project, 
+      :user => :user , 
+      :function => :function}.freeze
       include ActiveMessaging::MessageSender
       def send_msg(type, action, data)
         publish(:ossf_msg, 
-          "#{YAML::dump({'type' => type, 'action' => action, 'data' => data})}"
+          "#{YAML::dump({:type => type, :action => action, :data => data})}"
         )
       end
 
@@ -40,7 +40,7 @@ module OpenFoundry
      
       def send_msg(type, action, data)
         publish(:ossf_msg, 
-          "#{YAML::dump({'type' => type, 'action' => action, 'data' => data})}"
+          "#{YAML::dump({:type => type, :action => action, :data => data})}"
         )
       end
     end
