6a81801c6e16222fbf73f55847af60a580691554
Shawn C.H. Chiou <shawn-chiou@users.noreply.github.com>
2010-08-12 03:37:09 +0000

    Update with currently used on openfoundry production server

--- services/proftpd/sync_ftp_users.sh
+++ services/proftpd/sync_ftp_users.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
 PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin
-cd %%InstallDir%%;
-mysql -u %%DB_USER%% -p%%DB_PASS%% -h %%DB_HOST%% < sync_ftp_users.sql >> ftp_sync.log 2>&1 
+
+DB_USER="%%DB_USER%%"
+DB_PASS="%%DB_PASS%%"
+DB_HOST="%%DB_HOST%%"
+INSTALL_DIR="%%INSTALL_DIR%%"
+
+LOG_FILE="ftp_sync.log"
+SQL_FILE="sync_ftp_users.sql"
+LOCK_FILE="sync_ftp_users.lock"
+
+cd $INSTALL_DIR;
+
+if [ -f $LOCK_FILE ]; then
+	echo "lock on `date`" >> $MIRROR_NAME".log"
+else
+	echo "starting mirror on `date`" >> $LOG_FILE
+	touch $LOCK_FILE
+	mysql --default-character-set=utf8 -u $DB_USER -p$DB_PASS -h $DB_HOST < $SQL_FILE >> $LOG_FILE 2>&1 
+	rm $LOCK_FILE
+fi
