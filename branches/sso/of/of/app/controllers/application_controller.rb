b8fa4b83c0cf9ac798d9c8f543334751898fbb04
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2010-08-13 02:08:44 +0000

    fix session create many guest bug

--- branches/sso/of/of/app/controllers/application_controller.rb
+++ branches/sso/of/of/app/controllers/application_controller.rb
@@ -111,7 +111,7 @@ class ApplicationController < ActionController::Base
   
   #before_filter :login_required
   def current_user(session = session() )
-    session[:effective_user] || session[:user] || User.new(:login => 'guest')
+    session[:effective_user] || session[:user] || User.find_by_login('guest')
     #effective_user for site_admin 'su' to others 
     #user is your 'normal login user'
   end
