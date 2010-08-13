4a0fb70172c9768364ae8ae7e063cd596bbd08d0
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2010-08-13 02:09:44 +0000

    add mutex to prevent create users twice(thread safe)

--- branches/sso/of/of/app/metal/sync_data.rb
+++ branches/sso/of/of/app/metal/sync_data.rb
@@ -1,6 +1,7 @@
 # Allow the metal piece to run in isolation
 require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Rails)
 require 'action_pack'
+require 'thread'
 #
 #	post url
 # => http://ssodev.openfoundry.org/of/sync_data/ 
@@ -10,6 +11,7 @@ require 'action_pack'
 # => data	 : json data (login field is necessary) 
 #
 class SyncData
+  @@semaphore = Mutex.new
 
   def self.call(env)
 
@@ -21,24 +23,26 @@ class SyncData
 
 				case req.params["action"]
 				when "create"
-					if User.valid_users.find(:first, :conditions => {:login => userdata['name']})
-						[200, {"Content-Type" => "text/html"}, ["true"]]
-					else
-						syncuser = User.new(:login => userdata['name'], :verified => 1)
-						usertags = userdata.select{ |k, v| k=~ /^t_/ } || {}
-					
-						userdata.each { |k, v| syncuser.send(k + "=", v) if syncuser.respond_to? "#{k}=" }
-						if syncuser.save
-							#ActionController::send_msg(
-							#	ActionController::TYPES[:user],
-							#	ActionController::ACTIONS[:create],
-							#	{'id' => syncuser.id, 'name' => syncuser.login, 'email' => syncuser.email})
-							usertags.each { |k, v| syncuser.send(k + "=", v) }
-							[200, {"Content-Type" => "text/html"}, ["true"]] 
+ 				        @@semaphore.synchronize{
+						if User.valid_users.find(:first, :conditions => {:login => userdata['name']})
+							[200, {"Content-Type" => "text/html"}, ["true"]]
 						else
-							[200, {"Content-Type" => "text/html"}, ["false"]]
+							syncuser = User.new(:login => userdata['name'], :verified => 1)
+							usertags = userdata.select{ |k, v| k=~ /^t_/ } || {}
+						
+							userdata.each { |k, v| syncuser.send(k + "=", v) if syncuser.respond_to? "#{k}=" }
+							if syncuser.save
+								#ActionController::send_msg(
+								#	ActionController::TYPES[:user],
+								#	ActionController::ACTIONS[:create],
+								#	{'id' => syncuser.id, 'name' => syncuser.login, 'email' => syncuser.email})
+								usertags.each { |k, v| syncuser.send(k + "=", v) }
+								[200, {"Content-Type" => "text/html"}, ["true"]] 
+							else
+								[200, {"Content-Type" => "text/html"}, ["false"]]
+							end
 						end
-					end
+					}
 				when "update"
 					if syncuser	= User.valid_users.find(:first, :conditions => {:login => userdata['name']})
 						userdata.delete('name')
