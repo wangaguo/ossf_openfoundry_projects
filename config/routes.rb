013566c53792e70a402bf91e51bcada68f6e2f79
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2010-09-20 08:10:05 +0000

    update file with rails3

--- config/routes.rb
+++ config/routes.rb
@@ -1,184 +1,58 @@
-ActionController::Routing::Routes.draw do |map|
+Api::Application.routes.draw do
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
 
-  map.root :controller => 'openfoundry'
-  #####################
-  # API!!
-  #####################
-  map.resource :api,
-    :controller => :api, :only => [:user,:project],
-    :member => {:user => :get, :project => :get}
-  #####################
-  #administration:
-  #####################
-  map.connect 'site_admin', :controller => 'site_admin/site_admin'
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
 
-  map.namespace :site_admin do |admin|
-    admin.resource :admin, :controller => 'site_admin',
-		   :member => [:resend, :aaf_rebuild, :batch_add_users, :edit_code, :new_site_mail, :big_files]
-    admin.resources :projects,
-                    :member => {:change_status_form => :any,
-                                :list => :any,
-                                :projects_upload => :any,
-				:change_status => :any
-                                }
-    admin.resources :users
-    admin.resources :news
-  end
-  map.project_jobs '/projects/jobs', :controller => 'jobs', :action => 'project_jobs'
-  map.project_news '/projects/news', :controller => 'news', :action => 'project_news'
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
 
-  #####################
-  #downloaders' reviews: for project, release, and file
-  #####################
-  map.file_review '/projects/:id/releases/:version/files/:path/reviews',
-                      :controller => 'survey', :action => 'review',
-                      :requirements => {:path => /.+/, :version => /.+/}
-  map.release_review '/projects/:id/releases/:version/reviews',
-                      :controller => 'survey', :action => 'review',
-                      :requirements => {:version => /.+/}
-  map.project_review '/projects/:id/reviews', 
-                      :controller => 'survey', :action => 'review'
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
 
-  #####################
-  #resources:
-  #####################
-  map.resources :openfoundry,
-                :collection => { :search => :any, :download => :get, :is_project_name => :any,
-				 :foundry_dump => :any, :foundry_sync => :any,
-				 :redirect_rt_openfoundry_org => :any,
-				 :authentication_authorization => :any,
-				 :authentication_authorization_II => :any,
-				 :get_session_by_id => :any,
-				 :get_session_by_id2 => :any,
-				 :get_user_by_session_id => :any
-				}
-  map.resources :category
-  map.resources :projects,
-                :collection => { :list => :get, :applied => :get, 
-			         :tableizer => :get,
-                                 :new_projects_feed => :get},
-                :member => { :sympa => :get, :viewvc => :get, :websvn => :get, 
-			     :role_users => :any, 
-                             :member_edit => :any, :member_delete => :post,
-                             :member_add => :post, :permission_edit => :get,
-                             :member_change => :post, :role_update => :any,
-                             :group_update => :any,
-                             :group_create => :any, :group_delete => :get,
-                             :role_new => :any, :role_create => :any, :vcs_access => :any
-                             }
-  map.resources :news,
-                :singular => 'news1',
-                :collection => { :new_release => :any },
-                :path_prefix => '/projects/:project_id'
-  map.resources :news,
-                :singular => 'news1',
-                :collection => {:new_openfoundry_news_feed => :get,:new_project_news_feed => :get},
-                :name_prefix => 'site_'
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
 
-  #####################
-  #resources: jobs
-  #####################
-  map.resources :jobs,
-                :path_prefix => '/projects/:project_id'
-  map.connect '/jobs', :controller => :jobs, :action => :index
-  map.resources :jobs,
-                :collection => { :list => :get }
-  #####################
-  #resources: citations
-  #####################
-  map.resources :citations,
-                :path_prefix => '/projects/:project_id'
-  #####################
-  #resources: references
-  #####################
-  map.resources :references,
-                :path_prefix => '/projects/:project_id'
-  #####################
-  #resources: releases
-  #####################
-  map.resources :releases,
-    :path_prefix => '/projects/:project_id',
-    :collection => {:download => :any} ,
-    :member => { :uploadfiles => :any, :delete => :post, 
-                 :addfiles => :post, :removefile => :post,
-                 :editfile => :post, :updatefile => :post,
-                 :editrelease => :post, :updaterelease => :post,
-                 :viewrelease => :post, :viewfile => :post,
-                 :reload => :post,
-                 :web_upload => :post, :delete_files => :post,
-                 :download => :any, :new_releases => :any },
-    :singular => :release
-  map.resources :releases,
-    :collection => {:latest => :any, :top => :any,:new_release_feed => :get, :top_download_feed => :get }
-  #####################
-  #resources: other...
-  #####################
-  map.resources :kwiki,
-                :singular => 'kwiki1',
-                :path_prefix => '/projects/:project_id'
-  map.resources :rt,
-                :singular => 'rt1',
-		:collection => { :report => :get },
-                :path_prefix => '/projects/:project_id'
-  map.resources :rt,
-                :singular => 'rt1',
-                :name_prefix => 'site_'
-  map.resources :survey,
-                :path_prefix => '/projects/:project_id',
-                :member => {:update => :post, :apply => :any, :delete => :post}
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
 
-  map.resources :nscreports,
-                :controller => 'nscreports',
-                :path_prefix => '/projects/:project_id/nsc'
-  map.resources :images,
-                :only => [:cached_image, :upload, :email_image],
-                :collection => {:cached_image => :get, :upload => :any,
-                                :email_image => :get}
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
 
-  map.resource :user,
-    :only => [:logout, :login, :index],
-    :controller => :user,
-    :collection => {:dashboard => :get},
-    :member => {:home => :get,:index => :get,  
-                :login => :get, :logout => :get, 
-                :search => :post}
-  map.connect '/user', :controller => :user, :action => :index
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
 
-  map.resource :rescue,
-    :only => [:not_found],
-    :controller => :rescue,
-    :member => {:not_found => :get}
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
 
-  map.resources :webhosting,
-                :collection => { :how_to_upload => :get }
-  
-  map.resources :help,
-                :collection => { :index => :any, :nsc_project => :any, :vcs => :any }
+  # See how all your routes lay out with "rake routes"
 
-  #####################
-  #misc:
-  #####################
-  map.download1 '/projects/:project_id/download',
-    :controller => 'releases',
-    :action => 'download'  
-  # Install the default route as the lowest priority.
-  map.connect ':controller/:action/:id.:format'
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action.:format'
-
-
-  #for ~user home, eg: /~tim goes to :controller => :user, :id => 'tim' 
-  #map.connect '~:user_alias', :controller => 'user', :action => 'home'
-
-  #for downloader survey 
-  map.downloader 'download_path/:project_name/:release_version/:file_name/survey/:id',
-    :controller => 'survey',
-    :action => 'apply',
-    :requirements => {:file_name => /.+/, :release_version => /.+/}
-
-  #for download area~
-  map.download 'download_path/:project_name/:release_version/:file_name',
-    :controller => 'openfoundry',
-    :action => 'download',
-    :requirements => {:file_name => /.+/, :release_version => /.+/}
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id(.:format)))'
 end
