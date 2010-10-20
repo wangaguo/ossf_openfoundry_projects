c5bbfcfe348e8a921d51f649fad9486aa486801e
river <river@eab794a4-67ee-0310-8774-9eb661efe622>
2010-10-20 07:16:26 +0000

    New page for project category.

--- branches/category/of/app/controllers/category_controller.rb
+++ branches/category/of/app/controllers/category_controller.rb
@@ -18,20 +18,70 @@ class CategoryController < ApplicationController
   
   def list
     @module_name = _('Project Category')
-    #projects = Project.find(:all, :conditions => Project::in_used_projects())
-    @maturity = {}
-    @license = {}
-    @content_license = {}
-    @platform = {}
-    @programming_language = {}
-    
-    Project.in_used.each do |p|
-      [p.maturity].each{|x| @maturity[x] = (@maturity[x] || 0) + 1}
-      "#{p.license}".split(",").grep(/./).each{|x| @license[x] = (@license[x] || 0) + 1}
-      "#{p.contentlicense}".split(",").grep(/./).each{|x| @content_license[x] = (@content_license[x] || 0) + 1}
-      "#{p.platform}".split(",").grep(/./).each{|x| @platform[x] = (@platform[x] || 0) + 1}
-      "#{p.programminglanguage}".split(",").grep(/./).each{|x| @programming_language[x] = (@programming_language[x] || 0) + 1}
+#    #projects = Project.find(:all, :conditions => Project::in_used_projects())
+#    @maturity = {}
+#    @license = {}
+#    @content_license = {}
+#    @platform = {}
+#    @programming_language = {}
+#    
+#    Project.in_used.each do |p|
+#      [p.maturity].each{|x| @maturity[x] = (@maturity[x] || 0) + 1}
+#      "#{p.license}".split(",").grep(/./).each{|x| @license[x] = (@license[x] || 0) + 1}
+#      "#{p.contentlicense}".split(",").grep(/./).each{|x| @content_license[x] = (@content_license[x] || 0) + 1}
+#      "#{p.platform}".split(",").grep(/./).each{|x| @platform[x] = (@platform[x] || 0) + 1}
+#      "#{p.programminglanguage}".split(",").grep(/./).each{|x| @programming_language[x] = (@programming_language[x] || 0) + 1}
+    reset_sortable_columns
+    add_to_sortable_columns( 'listing', Project, 'name', 'name' )
+    add_to_sortable_columns( 'listing', Project, 'summary', 'summary' )
+    add_to_sortable_columns( 'listing', Project, 'category', 'category' )
+    add_to_sortable_columns( 'listing', Project, 'created_at', 'created_at' )
+
+		# all used projects
+		@pcs_projects = Project.in_used
+
+		# with category filter
+    params[ :cat ] = '0' if params[ :cat ].blank?
+    @pcs_projects = @pcs_projects.cated( params[ :cat ] )
+
+		# with search filter
+		unless params[ :cat_query ].blank?
+			keyword = params[ :cat_query ]
+			query = ( keyword + ' ' ).gsub( /([a-z0-9])+[\s]+/i ) { | m |
+				$0 = '';
+			  m.scan( /[a-z]+|\d+/i ).each{ | q |
+				  q.match( /^[a-z]+$/i ) ? $0 += " *#{q}* " : $0 += " #{q} "
+			  }
+		
+				$0
+			}
+
+			@options = {}
+			@options[ :per_page ] = 2000
+			@results = @pcs_projects.find_with_ferret( query, @options )
+			@pcs_projects = @results
+
+			# increase the search times for tags
+			Tagcloud.increase_searched_tag( keyword ) if session[ :search_keyword ] != keyword
+			session[ :search_keyword ] = keyword
+		end
+
+		# with nsc filter
+    @pcs_projects = @pcs_projects.select{ |p| p.is_nsc_project } if params[ :nsc_or_not ] == 'true'
+
+		# with paginate process
+    @final_project_list = nil
+    [ params[ :page ], 1 ].each do | page |
+      @final_project_list = @pcs_projects.paginate(
+		     :page => page,
+		     :per_page => 10,
+		     :include => [ :ready_releases ],
+		     :order => sortable_order( 'listing', :model => Release, :field => 'updated_at', :sort_direction => :desc ) ) 
+
+      break if not @final_project_list.out_of_bounds?
     end
+
+   	render :layout => false if request.xhr?
   end
 
   def show
