e4db0d29f898289414fbfba6ef379dc8c159a00d
kaworu <kaworu@eab794a4-67ee-0310-8774-9eb661efe622>
2010-10-22 06:41:56 +0000

    remove ferret, gettext, and message

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,14 +1,2 @@
 class Category < ActiveRecord::Base
-  # for breadcrumb i18n
-  N_("breadcrumb|vcs\_access")
-  N_("breadcrumb|my\_projects")
-  N_("breadcrumb|login")
-  N_("breadcrumb|new")
-  N_("breadcrumb|home")
-  N_("breadcrumb|edit")
-  N_("breadcrumb|owner")
-  N_("breadcrumb|creator")
-  N_("breadcrumb|requestor")
-  N_("breadcrumb|lastupdatedby")
-  N_("breadcrumb|site\_admin")
 end
