85100cc1cab1d2607c9953827a9fedd14a8f5d69
wangaguo <wangaguo@eab794a4-67ee-0310-8774-9eb661efe622>
2011-02-11 07:46:59 +0000

    OpenFoundry to Rails3 1. Igonre app & config/*.erb 2. Rails3 base 3. Gems & Plugins

--- branches/of_to_rails3/config/routes.rb
+++ branches/of_to_rails3/config/routes.rb
@@ -1,18 +1,19 @@
-ActionController::Routing::Routes.draw do |map|
+OpenFoundry::Application.routes.draw do |map|
 
-  map.root :controller => 'openfoundry'
+  base_url = Rails::Application.config.root_path
+  map.root :controller => 'openfoundry', :path_prefix => base_url 
   #####################
   # API!!
   #####################
   map.resource :api,
     :controller => :api, :only => [:user,:project],
-    :member => {:user => :get, :project => :get}
+    :member => {:user => :get, :project => :get}, :path_prefix => base_url
   #####################
   #administration:
   #####################
-  map.connect 'site_admin', :controller => 'site_admin/site_admin'
+  map.connect 'site_admin', :controller => 'site_admin/site_admin', :path_prefix => base_url
 
-  map.namespace :site_admin do |admin|
+  map.namespace :site_admin, :path_prefix => base_url+'/site_admin'  do |admin|
     admin.resource :admin, :controller => 'site_admin',
 		   :member => [:resend, :aaf_rebuild, :batch_add_users, :edit_code, :new_site_mail, :big_files]
     admin.resources :projects,
@@ -23,21 +24,22 @@ ActionController::Routing::Routes.draw do |map|
                                 }
     admin.resources :users
     admin.resources :news
+    admin.resources :tags
   end
-  map.project_jobs '/projects/jobs', :controller => 'jobs', :action => 'project_jobs'
-  map.project_news '/projects/news', :controller => 'news', :action => 'project_news'
+  map.project_jobs '/projects/jobs', :controller => 'jobs', :action => 'project_jobs', :path_prefix => base_url
+  map.project_news '/projects/news', :controller => 'news', :action => 'project_news', :path_prefix => base_url
 
   #####################
   #downloaders' reviews: for project, release, and file
   #####################
   map.file_review '/projects/:id/releases/:version/files/:path/reviews',
                       :controller => 'survey', :action => 'review',
-                      :requirements => {:path => /.+/, :version => /.+/}
+                      :requirements => {:path => /.+/, :version => /.+/}, :path_prefix => base_url
   map.release_review '/projects/:id/releases/:version/reviews',
                       :controller => 'survey', :action => 'review',
-                      :requirements => {:version => /.+/}
+                      :requirements => {:version => /.+/}, :path_prefix => base_url
   map.project_review '/projects/:id/reviews', 
-                      :controller => 'survey', :action => 'review'
+                      :controller => 'survey', :action => 'review', :path_prefix => base_url
 
   #####################
   #resources:
@@ -51,9 +53,9 @@ ActionController::Routing::Routes.draw do |map|
 				 :get_session_by_id => :any,
 				 :get_session_by_id2 => :any,
 				 :get_user_by_session_id => :any
-				}
+				}, :path_prefix => base_url
   map.resources :category,
-  		:collection => { :list => :get }
+  		:collection => { :list => :get }, :path_prefix => base_url
   map.resources :projects,
                 :collection => { :list => :get, :applied => :get, 
 			         :tableizer => :get,
@@ -66,43 +68,45 @@ ActionController::Routing::Routes.draw do |map|
                              :group_update => :any,
                              :group_create => :any, :group_delete => :any,
                              :role_new => :any, :role_create => :any, :vcs_access => :any
-                             }
+                             },
+                :path_prefix => base_url
+
   map.resources :news,
                 :singular => 'news1',
                 :collection => { :new_release => :any },
-                :path_prefix => '/projects/:project_id'
+                :path_prefix => base_url+'/projects/:project_id'
   map.resources :news,
                 :singular => 'news1',
                 :collection => {:new_openfoundry_news_feed => :get,:new_project_news_feed => :get},
-                :name_prefix => 'site_'
+                :name_prefix => 'site_', :path_prefix => base_url
 
   #####################
   #resources: jobs
   #####################
   map.resources :jobs,
-                :path_prefix => '/projects/:project_id'
-  map.connect '/jobs', :controller => :jobs, :action => :index
+                :path_prefix => base_url+'/projects/:project_id'
+  map.connect '/jobs', :controller => :jobs, :action => :index, :path_prefix => base_url
   map.resources :jobs,
-                :collection => { :list => :get }
+                :collection => { :list => :get }, :path_prefix => base_url
   #####################
   #resources: citations
   #####################
   map.resources :citations,
-                :path_prefix => '/projects/:project_id'
+                :path_prefix => base_url+'/projects/:project_id'
   #####################
   #resources: references
   #####################
   map.resources :references,
-                :path_prefix => '/projects/:project_id'
+                :path_prefix => base_url+'/projects/:project_id'
   #####################
   #resources: releases
   #####################
   map.resources :releases,
-    :path_prefix => '/projects/:project_id',
+    :path_prefix => base_url+'/projects/:project_id',
     :collection => {:download => :any} ,
     :member => { :uploadfiles => :any, :delete => :post, 
                  :addfiles => :post, :removefile => :post,
-                 :editfile => :post, :updatefile => :post,
+                 :editfile => :any, :updatefile => :post,
                  :editrelease => :post, :updaterelease => :post,
                  :viewrelease => :post, :viewfile => :post,
                  :reload => :post,
@@ -110,31 +114,31 @@ ActionController::Routing::Routes.draw do |map|
                  :download => :any, :new_releases => :any },
     :singular => :release
   map.resources :releases,
-    :collection => {:latest => :any, :top => :any,:new_release_feed => :get, :top_download_feed => :get }
+    :collection => {:latest => :any, :top => :any,:new_release_feed => :get, :top_download_feed => :get }, :path_prefix => base_url
   #####################
   #resources: other...
   #####################
   map.resources :kwiki,
                 :singular => 'kwiki1',
-                :path_prefix => '/projects/:project_id'
+                :path_prefix => base_url+'/projects/:project_id'
   map.resources :rt,
                 :singular => 'rt1',
 		:collection => { :report => :get },
-                :path_prefix => '/projects/:project_id'
+                :path_prefix => base_url+'/projects/:project_id'
   map.resources :rt,
                 :singular => 'rt1',
-                :name_prefix => 'site_'
+                :name_prefix => 'site_', :path_prefix => base_url
   map.resources :survey,
-                :path_prefix => '/projects/:project_id',
+                :path_prefix => base_url+'/projects/:project_id',
                 :member => {:update => :post, :apply => :any, :delete => :post}
 
   map.resources :nscreports,
                 :controller => 'nscreports',
-                :path_prefix => '/projects/:project_id/nsc'
+                :path_prefix => base_url+'/projects/:project_id/nsc'
   map.resources :images,
                 :only => [:cached_image, :upload, :email_image],
                 :collection => {:cached_image => :get, :upload => :any,
-                                :email_image => :get}
+                                :email_image => :get}, :path_prefix => base_url
 
   map.resource :user,
     :only => [:logout, :login, :index],
@@ -142,30 +146,30 @@ ActionController::Routing::Routes.draw do |map|
     :collection => {:dashboard => :get},
     :member => {:home => :get,:index => :get,  
                 :login => :get, :logout => :get, 
-                :search => :post}
-  map.connect '/user', :controller => :user, :action => :index
+                :search => :post}, :path_prefix => base_url
+  map.connect '/user', :controller => :user, :action => :index, :path_prefix => base_url
 
   map.resource :rescue,
     :only => [:not_found],
     :controller => :rescue,
-    :member => {:not_found => :get}
+    :member => {:not_found => :get}, :path_prefix => base_url
 
   map.resources :webhosting,
-                :collection => { :how_to_upload => :get }
+                :collection => { :how_to_upload => :get }, :path_prefix => base_url
   
   map.resources :help,
-                :collection => { :index => :any, :nsc_project => :any, :vcs => :any }
+                :collection => { :index => :any, :nsc_project => :any, :vcs => :any }, :path_prefix => base_url
 
   #####################
   #misc:
   #####################
   map.download1 '/projects/:project_id/download',
     :controller => 'releases',
-    :action => 'download'  
+    :action => 'download', :path_prefix => base_url
   # Install the default route as the lowest priority.
-  map.connect ':controller/:action/:id.:format'
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action.:format'
+  map.connect ':controller/:action/:id.:format', :path_prefix => base_url
+  map.connect ':controller/:action/:id', :path_prefix => base_url
+  map.connect ':controller/:action.:format', :path_prefix => base_url
 
 
   #for ~user home, eg: /~tim goes to :controller => :user, :id => 'tim' 
@@ -175,11 +179,11 @@ ActionController::Routing::Routes.draw do |map|
   map.downloader 'download_path/:project_name/:release_version/:file_name/survey/:id',
     :controller => 'survey',
     :action => 'apply',
-    :requirements => {:file_name => /.+/, :release_version => /.+/}
+    :requirements => {:file_name => /.+/, :release_version => /.+/}, :path_prefix => base_url
 
   #for download area~
   map.download 'download_path/:project_name/:release_version/:file_name',
     :controller => 'openfoundry',
     :action => 'download',
-    :requirements => {:file_name => /.+/, :release_version => /.+/}
+    :requirements => {:file_name => /.+/, :release_version => /.+/}, :path_prefix => base_url
 end
