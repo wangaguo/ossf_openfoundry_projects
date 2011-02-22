cccbcc713ffc0f021dc7d595ef18ed756ad15dc9
hyder <hyder@eab794a4-67ee-0310-8774-9eb661efe622>
2011-02-22 08:48:14 +0000

    Refactor OpenFoundry config files and of lib.

--- branches/of_to_rails3/config/environment.rb
+++ branches/of_to_rails3/config/environment.rb
@@ -2,4 +2,5 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
+ROOT_PATH = OpenFoundry::Application.config.root_path 
 OpenFoundry::Application.initialize!
