aac72a3b1626aeafdb64dc736e50eb17fed58aa5
wangaguo <wangaguo@eab794a4-67ee-0310-8774-9eb661efe622>
2011-03-23 08:03:53 +0000

    cititation save & error. the error is can't convert ActiveSupport::TimeWithZone into String. in the custom validate, release_date_before_type_cast is ActiveSupport::TimeWithZone. so, can need to_s like release_date_before_type_cast.to_s. but, change the validate use validates_timeliness gem.

--- trunk/of/app/models/citation.rb
+++ trunk/of/app/models/citation.rb
@@ -8,12 +8,5 @@ class Citation < ActiveRecord::Base
   validates_length_of :url, :within => 0..255, :too_long => _("Length range is ") + "0-255", :too_short => _("Length range is ") + "0-255"
   validates_length_of :release_version, :within => 0..255, :too_long => _("Length range is ") + "0-255", :too_short => _("Length range is ") + "0-255"
   validates_inclusion_of :status, :in => STATUS.values, :message => _("Not a valid value")
-  
-  def validate
-    if !release_date.nil?
-      date_pattern = /^(19|20)\d\d[- \/.](0?[1-9]|1[012])[- \/.](0?[1-9]|[12][0-9]|3[01])$/
-      errors.add(:release_date, "must be formatted YYYY-MM-DD or YYYY/MM/DD") if
-      date_pattern !~ release_date_before_type_cast
-    end
-  end
+  validates_exclusion_of :release_date, :in => [nil], :message => _("is an invalid datetime")
 end
