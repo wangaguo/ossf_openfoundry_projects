b61da749a4c1b774ee85de917b8e7d2060a59813
wangaguo <wangaguo@gmail.com>
2011-03-30 07:22:17 +0000

    Refine image mode & controller. perfect.

--- of/app/models/image.rb
+++ of/app/models/image.rb
@@ -11,8 +11,39 @@ class Image < ActiveRecord::Base
   validates_format_of :meta, :with => /^image/,
     :message => _("Image|Picture only")
 
+  # For convert to cache, need save source to temp.
   def save_to_file
-    File.open("#{IMAGE_DATA_DIR}/#{self.id}","w+").write(self.data)
+    f = File.open("#{IMAGE_DATA_DIR}/#{self.id}","w")
+    f.write(self.data)
+    f.close
+  end
+ 
+  # When image uploaded then convert 4 size to cache.
+  def convert_image
+    [16, 32, 64, 128].each do |size|
+      convert_to_cache(size)
+    end
+  end
+
+  # Convert image_size to cache.
+  def convert_to_cache(size)
+    image_cache_file = "#{Image::IMAGE_CACHES_DIR}/#{self.id}_#{size}"
+    meta = self.meta
+    unless File.exists?(image_cache_file)
+      image_data = "#{Image::IMAGE_DATA_DIR}/#{self.id}"
+      unless File.exists?(image_data)
+        save_to_file
+      end
+      ico = if(meta =~ /icon/); 'ico:' ;else '' ;end
+      if system("/usr/local/bin/convert #{ico}#{image_data}'[#{size}x#{size}]' #{image_cache_file}") == false
+        logger.error("image convert error. cmd: 'convert #{image_data}'[#{size}x#{size}]' #{image_cache_file}'")
+        false
+      else
+        true
+      end
+    else
+      false
+    end
   end
 
   def image=(picture_field)
