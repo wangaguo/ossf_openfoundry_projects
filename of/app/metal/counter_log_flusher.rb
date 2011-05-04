d55fceff84b1575c723a6776908bc841288f8260
wangaguo <wangaguo@gmail.com>
2011-05-04 06:44:42 +0000

    Refine the counter log. 1.wrong structure. 2.performance. 3.bulk insert. 4.transaction.

--- of/app/metal/counter_log_flusher.rb
+++ of/app/metal/counter_log_flusher.rb
@@ -1,17 +1,31 @@
 class CounterLogFlusher < ActionController::Metal
   def index
+    if params[:limit] 
+      if params[:limit].to_i <= 5000
+        limit = params[:limit].to_i
+      else
+        limit = 5000
+      end
+    else  
+      limit = 1000
+    end
+
+    logs = CounterLog.all.take(limit)
+    bulk_data = []
+    logs.each do |cl|
+      bulk_data << ArchivedCounterLog.new(:project_id => cl.project_id,
+                                  :release_id => cl.release_id,
+                                  :file_entity_id => cl.file_id,
+                                  :ip => cl.ip,
+                                  :created_at => Time.at(cl.created_at.to_i))
+    end
+
     ArchivedCounterLog.transaction do
-      CounterLog.all.each do |cl|
-        if cl.counter.item.is_a? Fileentity
-          f = cl.counter.item
-          ArchivedCounterLog.create(:project_id => f.release.project.id,
-                                    :release_id => f.release.id,
-                                    :file_entity_id => f.id,
-                                    :ip => cl.ip,
-                                    :created_at => Time.at(cl.created_at.to_i))
-        end
+      ArchivedCounterLog.import(bulk_data)
+      logs.each do |cl|
         cl.delete
       end
     end
   end
 end
+
