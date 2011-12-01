c17833cd95ca2c99b71ca82a234cba6b04af2c07
wangaguo <wangaguo@gmail.com>
2011-12-01 06:28:03 +0000

    create of to rails3 tag

--- releases/VCS for OpenFoundry/build/FreeBSD-x86-files.tcl
+++
@@ -1,5 +0,0 @@
-proc ::InstallJammer::InitFiles {} {
-    File ::265A9D1F-B262-0049-C27D-9AE4A8D8E51E -name config.sh -parent C73213B8-9061-3CB9-2E6B-4A9C7FFF000F -directory <%InstallDir%> -size 4051 -mtime 1207214464 -permissions 00755 -filemethod "Update files with more recent dates"
-    File ::2642F637-B5BB-5A2A-59D8-0015159628AF -name install.sh -parent C73213B8-9061-3CB9-2E6B-4A9C7FFF000F -directory <%InstallDir%> -size 3019 -mtime 1207820171 -permissions 00700 -filemethod "Update files with more recent dates"
-
-}
