78f44c849db305b89053500813f563d83b8c9c94
wangaguo <wangaguo@gmail.com>
2011-12-01 08:07:01 +0000

    branche a wiki2012

--- services/stompserver/stompserver.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-. %%InstallDir%%/ruby/ruby_settings.sh # TODO: ruby path
-stompserver -C %%InstallDir%%/stompserver.conf >> %%InstallDir%%/stompserver.log 2>&1 &
-echo $! > stompserver.pid
